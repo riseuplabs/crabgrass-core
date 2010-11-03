b82a39a5e9e825ef882b5972cbf5738052a6fdc8
Azul <azul@riseup.net>
2010-11-03 12:04:52 +0100

    separating permissions test from profile test

---
+++ test/unit/permissions_test.rb
@@ -0,0 +1,34 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class ProfileTest < ActiveSupport::TestCase
+
+  fixtures :users, :groups, :profiles, :permissions
+
+  def test_find_group
+    user = users(:red)
+
+    correct_visible_groups = Group.find(:all, :conditions => 'type IS NULL').select do |g|
+      user.may?(:view,g)
+    end
+    visible_groups = Group.visible_by(user).only_groups.find(:all)
+
+    correct_names = correct_visible_groups.collect{|g|g.name}.sort
+    names         = visible_groups.collect{|g|g.name}.sort
+
+    assert_equal  correct_names, names
+  end
+
+  def test_find_committee
+    user = users(:red)
+
+    correct_visible_groups = Committee.find(:all).select do |g|
+      user.may?(:view,g)
+    end
+    visible_groups = Committee.visible_by(user).find(:all)
+
+    correct_names = correct_visible_groups.collect{|g|g.name}.sort
+    names         = visible_groups.collect{|g|g.name}.sort
+
+    assert_equal  correct_names, names
+  end
+end
