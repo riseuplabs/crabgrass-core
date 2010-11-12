f0c8808f2de1c7c810051262f3342f6cd3d63980
Elijah Saxon <elijah@riseup.net>
2010-11-12 03:45:48 -0800

    added view_examples mod

---
+++ extensions/mods/view_examples/app/controllers/view_examples_controller.rb
@@ -0,0 +1,20 @@
+class ViewExamplesController < ApplicationController
+
+  #before_render :setup_navigation
+
+  def index
+    params[:file] ||= 'index'
+    setup_navigation
+    if RAILS_ENV == 'development'
+      render :file => 'view_examples/' + params[:file], :layout => 'application'
+    end
+  end
+
+  protected
+
+  def setup_navigation
+    @local_navigation_content = render_to_string :partial => 'view_examples/nav', :layout => false
+  end
+
+end
+
---
+++ extensions/mods/view_examples/config/routes.rb
@@ -0,0 +1,4 @@
+ActionController::Routing::Routes.draw do |map|
+  # map.examples :controller => 'examples'
+end
+
