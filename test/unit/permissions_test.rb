41eca597e7a0a800f3658d8c3842c6bfdfdb1505
Azul <azul@riseup.net>
2010-11-13 12:43:07 +0100

    It's the Permissions test now not Profile anymore

--- test/unit/permissions_test.rb
+++ test/unit/permissions_test.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-class ProfileTest < ActiveSupport::TestCase
+class PermissionsTest < ActiveSupport::TestCase
 
   fixtures :users, :groups, :profiles, :permissions
 
