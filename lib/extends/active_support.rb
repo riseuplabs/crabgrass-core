79df4e94f57afbb171eddec19cddc1dc58f7caef
Azul <azul@riseup.net>
2010-11-13 12:46:16 +0100

    no more need for the path aliases as we require 2.3.10 anyway

--- lib/extends/active_support.rb
+++
@@ -1,8 +0,0 @@
-module ActiveSupport::Dependencies
-  unless ActiveSupport::Dependencies.method_defined? :autoload_paths
-    alias_method :autoload_paths, :load_paths
-    alias_method :autoload_paths=, :load_paths=
-    alias_method :autoload_once_paths, :load_once_paths
-    alias_method :autoload_once_paths=, :load_once_paths=
-  end
-end
--- lib/extends/railties.rb
+++
@@ -1,8 +0,0 @@
-class Rails::Configuration
-  unless Rails::Configuration.method_defined? :autoload_paths
-    alias_method :autoload_paths, :load_paths
-    alias_method :autoload_paths=, :load_paths=
-    alias_method :autoload_once_paths, :load_once_paths
-    alias_method :autoload_once_paths=, :load_once_paths=
-  end
-end
