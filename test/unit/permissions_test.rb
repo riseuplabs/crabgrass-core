2c4b6069a20baeccf1381276e619b3e4f42a0408
Azul <azul@riseup.net>
2010-11-14 10:55:13 +0100

    testing basic committee actions with permissions

--- test/unit/permissions_test.rb
+++ test/unit/permissions_test.rb
@@ -2,15 +2,45 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class PermissionsTest < ActiveSupport::TestCase
 
-  fixtures :users, :groups, :profiles, :permissions
 
-  def test_find_group
-    user = users(:red)
+  def test_group_permissions_with_committees
+    user = User.make
+    group = Group.make_owned_by(:user => user)
+    group.add_user! user
+    invisible = Group.make
+
+    assert user.may?(:admin, group), "should admin group i'm in"
+    assert !user.may?(:view, invisible), "should not view group i'm not in."
+
+    committee = Committee.make_for :group => group
+    assert user.may?(:admin, committee), "should admin committee of my group."
+    council = Council.make_for :group => group
+    group.reload
+    committee.reload
+    user.clear_access_cache
+    assert !user.may?(:admin, committee), "should not admin committee of group with council."
+    assert !user.may?(:admin, group)
+    assert user.may?(:view, committee)
+    assert user.may?(:view, group)
+
+    council.add_user! user
+    group.reload
+    committee.reload
+    user.clear_access_cache
+
+    assert user.may?(:admin, committee)
+    assert user.may?(:admin, group)
+
+    invisible.allow!(:all, [:view, :pester])
+    user.clear_access_cache
+    invisible.reload
+    assert user.may?(:pester, invisible)
+    assert !user.may?(:admin, invisible)
 
     correct_visible_groups = Group.find(:all, :conditions => 'type IS NULL').select do |g|
       user.may?(:view,g)
     end
-    visible_groups = Group.with_access(:see, user).only_groups.find(:all)
+    visible_groups = Group.with_access(:view, user).only_groups.find(:all)
 
     correct_names = correct_visible_groups.collect{|g|g.name}.sort
     names         = visible_groups.collect{|g|g.name}.sort
@@ -24,7 +54,7 @@ class PermissionsTest < ActiveSupport::TestCase
     correct_visible_groups = Committee.find(:all).select do |g|
       user.may?(:view,g)
     end
-    visible_groups = Committee.visible_by(user).find(:all)
+    visible_groups = Committee.with_access(:view, user).find(:all)
 
     correct_names = correct_visible_groups.collect{|g|g.name}.sort
     names         = visible_groups.collect{|g|g.name}.sort
