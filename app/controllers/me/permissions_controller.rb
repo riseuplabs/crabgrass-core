afcf2f4f70c015b52318c6887104deecd52c3a45
Azul <azul@riseup.net>
2010-11-24 10:31:39 +0100

    initial views and controller for me/permissions

--- app/controllers/me/permissions_controller.rb
+++ app/controllers/me/permissions_controller.rb
@@ -4,32 +4,20 @@ class Me::PermissionsController < Me::BaseController
     @permissions = current_user.permissions #.without_self
   end
 
-  def show
-    @permission = current_user.permissions.find params[:id]
-  end
-
-  def new
-    @permission = Permission.new
-  end
-
   def create
-    @permission = Permission.new params[:permission]
-    if @permission.save
-      redirect_to @permission, :notice => 'Permission added'
+    @permission = current_user.allow! params[:permission][:entity_code], :view
+    if @permission
+      redirect_to me_permissions_url, :notice => 'Permission added'
     else
       render :action => :new
     end
   end
 
-  def edit
-    @permission = current_user.permissions.find params[:id]
-  end
-
   def update
-    @permission = current_user.permission.find params[:id]
+    @permission = current_user.permissions.find params[:id]
     @permission.update_attributes! params[:permission]
     success
-    redirect_to @permission
+    redirect_to me_permissions_url
   end
 
   def destroy
