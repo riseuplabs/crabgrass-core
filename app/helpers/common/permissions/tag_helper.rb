ddde8b84ad03ef56667c9f3c0e20804df552fed9
Azul <azul@riseup.net>
2011-05-30 16:44:40 +0200

    permissions switch shows unchecked checkboxes

--- app/helpers/common/permissions/tag_helper.rb
+++ app/helpers/common/permissions/tag_helper.rb
@@ -6,26 +6,40 @@
 
 module Common::Permissions::TagHelper
 
-  def permission_switch_tag(context, holder, lock, checked = true)
-    label = case holder
-      when Symbol
-        holder
-      else
-        holder.display_name
+  def permission_switch_tag(context, key, lock)
+    name    = "#{key.holder.to_sym}_#{lock}"
+    label   = label_for_holder(key.holder)
+    url     = switch_url(context, key.holder)
+    options = switch_options(key, lock)
+    spinbox_tag(name, label, url, options)
+  end
+
+  private
+
+  def label_for_holder(holder)
+    case holder
+    when Symbol, String
+      holder
+    else
+      holder.display_name
     end
+  end
+
+  def switch_url(context, holder)
     code = ActsAsLocked::Key.code_for_holder(holder)
-    url = case context
+    case context
       when :me
         me_permission_path(code)
       when Group
         group_permission_path(context, code)
     end
+  end
+
+  def switch_options(key, lock)
+    checked = key.opens? lock
     options = {:checked => checked,
       :with => "'#{lock}=' + '#{!checked}'",
       :method => 'put'
     }
-    name = holder.to_sym.to_s + '_' + lock.to_s
-    spinbox_tag(name, label, url, options)
   end
-
 end
