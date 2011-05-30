a7a4c97eb0c5dd252e6805e9179133d1a6267803
Azul <azul@riseup.net>
2011-05-30 21:36:24 +0200

    removed some outdated functions in permissions controller

--- app/controllers/me/permissions_controller.rb
+++ app/controllers/me/permissions_controller.rb
@@ -5,26 +5,11 @@ class Me::PermissionsController < Me::BaseController
     @locks = User.locks
   end
 
-  def create
-    @permission = current_user.grant! params[:permission][:keyring_code], :view
-    if @permission
-      redirect_to me_permissions_url, :notice => 'Permission added'
-    else
-      render :action => :new
-    end
-  end
-
   def update
     @keys = current_user.keys
     @key = @keys.find_by_keyring_code params.delete(:id)
     @locks = @key.update!(params)
     success
   end
-
-#  def destroy
-#    @permission = current_user.permissions.find params[:id]
-#    @permission.destroy
-#    redirect_to me_permissions_url
-#  end
 
 end
