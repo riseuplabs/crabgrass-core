9a608a2ad12318b36bd81ccc0492ab7d35c47ef8
elijah <elijah@riseup.net>
2011-06-09 21:09:45 -0700

    test: me requests

--- test/functional/me/requests_controller_test.rb
+++ test/functional/me/requests_controller_test.rb
@@ -5,11 +5,10 @@ class Me::RequestsControllerTest < ActionController::TestCase
   fixtures :users, :requests
 
   def test_destroy
-    flunk 'not yet done as templates are in weird spot. also regexp should be fixed'
     login_as users(:blue)
     request = Request.created_by(users(:blue)).find(:first)
     xhr :delete, :destroy, :id => request.id
-    assert_message /skdjhsf/i
+    assert_message /destroyed/i
   end
 
 end
