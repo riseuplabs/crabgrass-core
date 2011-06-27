0c8767f57a327b76409bcafd1739ccf122df7357
jessi <jessib@riseup.net>
2011-06-27 16:03:19 -0700

    Improvements to people directory.

--- app/controllers/people/directory_controller.rb
+++ app/controllers/people/directory_controller.rb
@@ -1,8 +1,27 @@
 class People::DirectoryController < ApplicationController
 
+# will need permissions, pagination, improved display
+
   def index
-    @users = User.all
+    if friends?
+      @users = current_user.friends.paginate(pagination_params)
+    elsif peers?
+      @users = current_user.peers.paginate(pagination_params)
+    else
+      @users = User.paginate(pagination_params)
+    end
+  end
+  
+  protected
+
+#  VIEW_KEYWORDS = ['friends', 'peers']
+
+  def friends?
+    params[:path].try.include? 'friends'
   end
 
+  def peers?
+    params[:path].try.include? 'peers'
+  end
 end
 
