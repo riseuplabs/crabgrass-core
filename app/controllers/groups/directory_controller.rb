e4eab532f799527dd6b1b4a6365c6330d35d5b22
jessib <jessib@riseup.net>
2011-06-30 12:02:06 -0700

    Set up to have directory controller work like people controller. HOwever, currently acts_as_site_limited messed up pagination.

--- app/controllers/groups/directory_controller.rb
+++ app/controllers/groups/directory_controller.rb
@@ -1,7 +1,11 @@
 class Groups::DirectoryController < ApplicationController
 
   def index
-    @groups = Group.paginate(pagination_params)
+    if !logged_in?
+      @groups = Group.access_by(:public).allows(:view).paginate(pagination_params)
+    else
+      @groups = Group.access_by(current_user).allows(:view).paginate(pagination_params)
+    end
   end
 
 end
