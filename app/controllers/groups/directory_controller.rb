2527c285a326278661ac6ad3010acdb2679e9087
jessi <jessib@riseup.net>
2011-07-04 15:43:20 -0700

    Show directory of my groups correctly.

--- app/controllers/groups/directory_controller.rb
+++ app/controllers/groups/directory_controller.rb
@@ -3,9 +3,17 @@ class Groups::DirectoryController < ApplicationController
   def index
     if !logged_in?
       @groups = Group.access_by(:public).allows(:view).paginate(pagination_params)
+    elsif my_groups?
+      @groups = current_user.groups.paginate(pagination_params)
     else
       @groups = Group.access_by(current_user).allows(:view).paginate(pagination_params)
     end
+  end
+
+  protected
+  
+  def my_groups?
+    params[:path].try.include? 'my'
   end
 
 end
