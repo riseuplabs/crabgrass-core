ff5be24cfbd4d271fa2773da6f8801c2319521fb
jessib <jessib@riseup.net>
2011-08-01 17:39:43 -0700

    Some work on directories, which does not yet display pretty.

--- app/controllers/groups/directory_controller.rb
+++ app/controllers/groups/directory_controller.rb
@@ -2,16 +2,17 @@ class Groups::DirectoryController < ApplicationController
 
   def index
     if !logged_in?
-      @groups = Group.access_by(:public).allows(:view).paginate(pagination_params)
+      @groups = Group.access_by(:public).allows(:view).find_all_by_type(nil).paginate(pagination_params) # won't show committees or networks
     elsif my_groups?
-      @groups = current_user.groups.paginate(pagination_params)
+      @groups = current_user.primary_groups.paginate(pagination_params) #this just paginates on primary groups. we will still show committees underneath. primary_groups are groups, or committees when we don't belong to the parent group
     else
-      @groups = Group.access_by(current_user).allows(:view).paginate(pagination_params)
+      @groups = Group.access_by(current_user).allows(:view).find_all_by_type(nil).paginate(pagination_params) # find_all_by_type will exclude committees or networks
     end
   end
 
   protected
-  
+  helper_method :my_groups?
+
   def my_groups?
     params[:path].try.include? 'my'
   end
