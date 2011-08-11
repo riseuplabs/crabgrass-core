6b56a8013a4dd7e461ea14e71e6a7fb3d974b50c
jessib <jessib@riseup.net>
2011-08-10 19:57:36 -0700

    Fix problems with form when creating a new group or committee (https://labs.riseup.net/code/issues/3332)

--- app/controllers/groups/committees_controller.rb
+++ app/controllers/groups/committees_controller.rb
@@ -5,10 +5,15 @@ class Groups::CommitteesController < Groups::BaseController
   end
 
   def create
-    @committee = Committee.create params[:committee].merge(:created_by => current_user)
-    @group.add_committee!(@committee)
-    redirect_to group_url(@committee)
-    success
+    @committee = Committee.new params[:committee].merge(:created_by => current_user)
+    @committee.save!
+    if @committee.valid?
+      @group.add_committee!(@committee)
+      redirect_to group_url(@committee)
+      success
+    else
+      redirect_to(params[:redirect])
+    end
   end
 
 end
