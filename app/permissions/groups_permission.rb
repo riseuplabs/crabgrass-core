de7feb4e4385a8185823b37d16f0d77d726a96a6
Azul <azul@riseup.net>
2011-08-16 17:13:05 +0200

    one groups permission to include them all

---
+++ app/permissions/groups_permission.rb
@@ -0,0 +1,11 @@
+module GroupsPermission
+
+  prefix = File.dirname(__FILE__)
+  path = prefix + '/groups'
+  file_paths = Dir.glob(path + '/*.rb').collect{|f|f.chomp('.rb')}
+  file_paths.each do |file_path|
+    relative_path = file_path.sub(/^#{Regexp.escape(prefix)}/, "")
+    include(relative_path.camelize.constantize)
+  end
+
+end
