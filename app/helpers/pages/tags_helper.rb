9e691d9de312eb0a7e645435f062390fdfa7b928
elijah <elijah@riseup.net>
2011-08-16 22:21:36 -0700

    page tags: stuff i forgot to checkin regarding page tags.

--- app/helpers/pages/tags_helper.rb
+++ app/helpers/pages/tags_helper.rb
@@ -2,13 +2,16 @@ module Pages::TagsHelper
 
   def remove_tag_link(tag)
     link = link_to_remote(
-      I18n.t(:delete),
+      :remove.t,
       { :url => page_tag_path(@page, tag.name),
         :method => :delete,
         :complete => hide("tag_#{tag.id}") },
-      { :class => 'shy', :icon => 'tiny_trash'}
+      { :class => 'shy inline', :icon => 'tiny_trash'}
     )
-    content_tag(:p, h(tag.name) + ' ' + link, :id => "tag_#{tag.id}", :class => 'shy_parent small_icon tag_16')
+    content_tag(:div, :id => "tag_#{tag.id}", :class => 'shy_parent p') do 
+      content_tag(:span, h(tag.name), :class => 'icon tag_16 inline') + ' ' +
+      link
+    end
   end
 
   def options_for_edit_tag_form
@@ -18,8 +21,6 @@ module Pages::TagsHelper
       :page_id => @page.id,
       :html     => {:id => 'edit_tag_form'},
       :loading  => show_spinner('tag')
-#      :complete => hide_spinner('tag')
-#      :success  => reset_form('edit_tag_form')
     }]
   end
 
