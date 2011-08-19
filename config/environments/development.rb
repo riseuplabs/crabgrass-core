fc61c2e6b8975477ef6b148bc49e846e5a98768c
elijah <elijah@riseup.net>
2011-08-19 02:24:43 -0700

    Now, in dev mode, config.reload_plugins is enabled. Not sure if this will do the trick, but it is supposed to reload plugins when they change.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,11 +9,18 @@ config.action_view.debug_rjs                         = true
 config.action_controller.perform_caching             = false
 config.action_mailer.raise_delivery_errors = false
 config.log_level = :debug
+config.reload_plugins = true
 
 ##
 ## CRABGRASS OPTIONS
 ##
 
+# 
+# When running crabgrass, you can set the environment variable
+# INFO to a level 0 .. 10 to print out debugging messages.
+# This sets the default level to 0, which shows the high level
+# messages.
+#
 DEFAULT_INFO_LEVEL = 0
 
 ##
@@ -22,5 +29,6 @@ DEFAULT_INFO_LEVEL = 0
 ##
 
 require "#{RAILS_ROOT}/lib/crabgrass/debug.rb"
+
 
 
