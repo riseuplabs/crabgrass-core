e773731a902aec152d07f3bce4adf617a6551117
elijah <elijah@riseup.net>
2011-08-19 02:24:00 -0700

    people/profile: fixed icons, let users see themselves fully.

--- app/permissions/people_permission.rb
+++ app/permissions/people_permission.rb
@@ -3,11 +3,11 @@ module PeoplePermission
   protected
 
   def may_list_friends?
-    current_user.may?(:see_contacts, @user)
+    current_user.may?(:see_contacts, @user) or current_user == @user
   end
 
   def may_list_groups?
-    current_user.may?(:see_groups, @user)
+    current_user.may?(:see_groups, @user) or current_user == @user
   end
 
   def may_request_contact?
