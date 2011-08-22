d6bdb5342e7d51c2aabf7daeeec9eac4ef8d8c31
Azul <azul@riseup.net>
2011-08-22 10:37:10 +0200

    fixed tests to match new permissions

--- test/unit/activity_test.rb
+++ test/unit/activity_test.rb
@@ -1,143 +1,133 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class ActivityTest < ActiveSupport::TestCase
-  fixtures :users, :groups, :activities, :memberships, :federatings
+
+  def setup
+    @joe = User.make
+    @ann = User.make
+    @group = Group.make
+    @group.add_user! @joe
+    @group.add_user! @ann
+    @joe.reload
+    @ann.reload
+  end
 
   def test_contact
-    u1 = users(:kangaroo)
-    u2 = users(:iguana)
+    @joe.add_contact!(@ann, :friend)
 
-    u1.add_contact!(u2, :friend)
-
-    act = FriendActivity.for_all(u1).find(:first)
+    act = FriendActivity.for_all(@joe).find(:first)
     assert act, 'there should be a friend activity created'
-    assert_equal u1, act.user
-    assert_equal u2, act.other_user
+    assert_equal @joe, act.user
+    assert_equal @ann, act.other_user
   end
 
   def test_user_destroyed
-    u1 = users(:kangaroo)
-    u2 = users(:iguana)
 
-    assert u1.peer_of?(u2)
-    username = u2.name
-    u2.destroy
+    assert @joe.peer_of?(@ann)
+    username = @ann.name
+    @ann.destroy
 
-    act = UserDestroyedActivity.for_all(u1).find(:first)
+    act = UserDestroyedActivity.for_all(@joe).find(:first)
     assert act, 'there should be a user destroyed activity created'
     assert_equal username, act.username
   end
 
   def test_group_destroyed
-    user = users(:kangaroo)
-    group = groups(:animals)
+    groupname = @group.name
+    @group.destroy_by(@joe)
 
-    assert user.member_of?(group)
-    groupname = group.name
-    group.destroy_by(user)
-
-    acts = Activity.for_all(user).find(:all)
+    acts = Activity.for_all(@joe).find(:all)
     act = acts.detect{|a|a.class == GroupDestroyedActivity}
     assert_equal groupname, act.groupname
-    assert_in_description(act, group)
+    assert_in_description(act, @group)
   end
 
   def test_group_created
-    user = users(:green)
-    notified_user = users(:kangaroo)
     group = Group.create!(:name => "plants",
                           :fullname =>"All the plants") do |group|
       group.avatar = Avatar.new
-      group.created_by = user
+      group.created_by = @ann
     end
     act = GroupCreatedActivity.find(:last)
-    assert_activity_for_user_group(act, user, group)
+    assert_activity_for_user_group(act, @ann, group)
 
     act = UserCreatedGroupActivity.find(:last)
-    assert_activity_for_user_group(act, user, group)
+    assert_activity_for_user_group(act, @ann, group)
     assert_equal group.id, act.group.id
-    assert_equal user.id, act.user.id
+    assert_equal @ann.id, act.user.id
     assert_in_description(act, group)
-    assert_in_description(act, user)
+    assert_in_description(act, @ann)
   end
 
   def test_membership
-    group = groups(:animals)
-    user = users(:green)
-    notified_user = users(:kangaroo)
+    ruth = User.make
+    @group.add_user!(ruth)
 
-    ##
-    ## Add the user
-    ##
+    assert_nil UserJoinedGroupActivity.for_all(@ann).find_by_subject_id(ruth.id),
+      "The new peers don't get UserJoinedGroupActivities."
 
-    group.add_user!(user)
+    act = GroupGainedUserActivity.for_all(@ann).last
+    assert_equal @group.id, act.group.id,
+      "New peers should get GroupGainedUserActivities."
 
-    assert_nil UserJoinedGroupActivity.for_all(notified_user).find_by_subject_id(user.id), 'the notified user does not get this kind of activity message'
-
-    act = GroupGainedUserActivity.for_all(notified_user).last
-    assert_equal group.id, act.group.id, 'the notified user should get this activity'
-
-    act = GroupGainedUserActivity.for_group(group, notified_user).last
+    act = GroupGainedUserActivity.for_group(@group, ruth).last
     assert_equal GroupGainedUserActivity, act.class
-    assert_equal group.id, act.group.id
+    assert_equal @group.id, act.group.id
 
     # users own activity should always show up:
-    act = UserJoinedGroupActivity.for_all(user).last
-    assert_equal group.id, act.group.id
+    act = UserJoinedGroupActivity.for_all(ruth).last
+    assert_equal @group.id, act.group.id
 
     ##
     ## Remove the user
     ##
 
-    group.remove_user!(user)
+    @group.remove_user!(ruth)
 
-    act = GroupLostUserActivity.for_all(notified_user).last
-    assert_activity_for_user_group(act, user, group)
+    act = GroupLostUserActivity.for_all(@ann).last
+    assert_activity_for_user_group(act, ruth, @group)
 
-    act = GroupLostUserActivity.for_group(group, notified_user).last
-    assert_activity_for_user_group(act, user, group)
+    act = GroupLostUserActivity.for_group(@group, @ann).last
+    assert_activity_for_user_group(act, ruth, @group)
 
-    act = UserLeftGroupActivity.for_all(user).last
-    assert_activity_for_user_group(act, user, group)
+    act = UserLeftGroupActivity.for_all(ruth).last
+    assert_activity_for_user_group(act, ruth, @group)
   end
 
   def test_deleted_subject
-    u1 = users(:kangaroo)
-    u2 = users(:iguana)
-
-    u1.add_contact!(u2, :friend)
-    act = FriendActivity.for_all(u1).find(:first)
-    u2.destroy
+    @joe.add_contact!(@ann, :friend)
+    act = FriendActivity.for_all(@joe).find(:first)
+    former_name = @ann.name
+    @ann.destroy
 
     assert act, 'there should be a friend activity created'
     assert_equal nil, act.other_user
-    assert_equal 'iguana', act.other_user_name
-    assert_equal '<span class="user">iguana</span>', act.user_span(:other_user)
+    assert_equal former_name, act.other_user_name
+    assert_equal "<span class=\"user\">#{former_name}</span>",
+      act.user_span(:other_user)
   end
 
   def test_avatar
-    group = groups(:rainbow)
-    u1 = users(:kangaroo)
-    u2 = users(:iguana)
+    new_group = Group.make
 
-    u1.add_contact!(u2, :friend)
-    u1.send_message_to!(u2, "hi u2")
-    group.add_user!(u1)
+    @joe.add_contact!(@ann, :friend)
+    @joe.send_message_to!(@ann, "hi @ann")
+    new_group.add_user!(@joe)
 
-    friend_act = FriendActivity.find_by_subject_id(u1.id)
-    user_joined_act = UserJoinedGroupActivity.find_by_subject_id(u1.id)
-    group_gained_act = GroupGainedUserActivity.find_by_subject_id(group.id)
-    post_act = PrivatePostActivity.find_by_subject_id(u2.id)
+    friend_act = FriendActivity.find_by_subject_id(@joe.id)
+    user_joined_act = UserJoinedGroupActivity.find_by_subject_id(@joe.id)
+    group_gained_act = GroupGainedUserActivity.find_by_subject_id(new_group.id)
+    post_act = PrivatePostActivity.find_by_subject_id(@ann.id)
     # we do not create PrivatePostActivities anymore
     assert_nil post_act
 
 
     # the person doing the thing should be the avatar for it
     # disregarding whatever is the subject (in the gramatical/language sense) of the activity
-    assert_equal u1, friend_act.avatar
-    assert_equal u1, user_joined_act.avatar
-    assert_equal u1, group_gained_act.avatar
-    #assert_equal u1, post_act.avatar
+    assert_equal @joe, friend_act.avatar
+    assert_equal @joe, user_joined_act.avatar
+    assert_equal @joe, group_gained_act.avatar
+    #assert_equal @joe, post_act.avatar
   end
 
   def test_associations
