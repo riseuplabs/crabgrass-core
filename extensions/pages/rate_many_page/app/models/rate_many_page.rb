2a0ac5e949351ba99a4aecf252428e92082e80bc
jessib <jessib@riseup.net>
2011-08-23 17:07:35 -0700

    Adding rate many page functionality.

--- extensions/pages/rate_many_page/app/models/rate_many_page.rb
+++ extensions/pages/rate_many_page/app/models/rate_many_page.rb
@@ -1,9 +1,24 @@
 class RateManyPage < Page
-  validates_presence_of :data, :on => :create, :message => "can't be blank"
-  # Return string of all rate_many possibiles, for the full text search index
+
+  before_validation :create_poll
+
   def body_terms
-    return "" unless data and data.possibles
-    data.possibles.collect { |pos| "#{pos.name}\t#{pos.description}" }.join "\n"
+    return "" unless poll and poll.possibles
+    poll.possibles.collect { |pos| "#{pos.name}\t#{pos.description}" }.join "\n"
+  end
+
+  alias_method :poll, :data
+
+  protected
+
+  #
+  # create the RatingPoll object if it does not already exist
+  #
+  def create_poll
+    unless self.data_id
+      self.data = RatingPoll.new
+    end
+    return true # ensure we don't halt on this callback
   end
 
 end
