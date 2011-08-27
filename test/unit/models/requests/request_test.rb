ae406a03085792d0057903f4d738bb22a56bf0ce
Azul <azul@riseup.net>
2011-08-27 17:42:24 +0200

    made request test faster by not saving the request

--- test/unit/models/requests/request_test.rb
+++ test/unit/models/requests/request_test.rb
@@ -3,16 +3,17 @@ require File.expand_path(File.dirname(__FILE__) + '/test_helper')
 class RequestTest < ActiveSupport::TestCase
 
   def test_create_checks_permissions
-    user = User.make
-    request = Request.new :created_by => user
+    user = stub
+    request = Request.new
+    request.stubs(:created_by).returns(user)
     request.expects(:may_create?).with(user).returns(false)
     assert !request.save
   end
 
   def test_mark_as_destroy_checks_permission
-    user = User.make
-    request = Request.new :created_by => user
-    request.save_with_validation(false)
+    user = stub(:name => 'Dr. Stub')
+    request = Request.new
+    request.stubs(:new_record?).returns(false)
     assert_raises PermissionDenied do
       request.expects(:may_destroy?).with(user).returns(false)
       request.mark! :destroy, user
@@ -20,9 +21,9 @@ class RequestTest < ActiveSupport::TestCase
   end
 
   def test_mark_as_approved_checks_permission
-    user = User.make
-    request = Request.new :created_by => user
-    request.save_with_validation(false)
+    user = User.new
+    request = Request.new :state => 'pending'
+    request.stubs(:new_record?).returns(false)
     assert_raises PermissionDenied do
       request.expects(:may_approve?).with(user).returns(false)
       request.mark! :approve, user
@@ -30,9 +31,9 @@ class RequestTest < ActiveSupport::TestCase
   end
 
   def test_mark_as_rejected_checks_permission
-    user = User.make
-    request = Request.new :created_by => user
-    request.save_with_validation(false)
+    user = User.new
+    request = Request.new :state => 'pending'
+    request.stubs(:new_record?).returns(false)
     assert_raises PermissionDenied do
       request.expects(:may_approve?).with(user).returns(false)
       request.mark! :reject, user
