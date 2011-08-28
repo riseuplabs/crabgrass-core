7069948345cd78dbbea9dfa5e2a06ad2ee3e8232
Azul <azul@riseup.net>
2011-08-28 15:25:01 +0200

    fixes from the merge we now use may_create_group_committe? with a singular group

--- app/permissions/groups/affiliations_permission.rb
+++ app/permissions/groups/affiliations_permission.rb
@@ -2,19 +2,19 @@ module Groups::AffiliationsPermission
 
   protected
 
-  def may_create_groups_committee?(group=@group)
+  def may_create_group_committee?(group=@group)
     return false if !Conf.committees
     current_user.may?(:admin, group) and group.parent_id.nil?
   end
 
   # (must accept optional argument)
-  def may_list_groups_committees?(group = @group)
+  def may_list_group_committees?(group = @group)
     return false if !Conf.committees
     return false if group.parent_id
     current_user.may? :see_committees, group
   end
 
-  def may_list_groups_networks?(group = @group)
+  def may_list_group_networks?(group = @group)
     return false if !Conf.networks
     current_user.may? :see_networks, group
   end
