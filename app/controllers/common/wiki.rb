90d5e195905e040ceeebc7e41ca73c8ef82b5920
Azul <azul@riseup.net>
2011-11-04 15:36:24 +0100

    cleanup and clearifications

--- app/controllers/common/wiki.rb
+++ app/controllers/common/wiki.rb
@@ -39,42 +39,26 @@ module Common::Wiki
       @wiki = @profile.create_wiki(:version => 0, :body => params[:wiki][:body])
     end
     #TODO: need to unlock when cancelling, as will still be locked
-    #todo--i would think create.rjs would not work right when cancelling but it seems okay. specifically, i would think it would always toggle the public wiki, but it doesn't seem to.
+    #TODO: i would think create.rjs would not work right when cancelling but it seems okay. specifically, i would think it would always toggle the public wiki, but it doesn't seem to.
 
     #render :template => '/common/wiki/show' #redirect doesn't work correctly in firefox 3.6.23 (and maybe other versions), so we will render template
   end
 
   def edit
     if params[:break_lock]
-      @wiki.unlock!(:document, current_user, :break => true ) #seems funny to unlock then to lock?
+      # remove other peoples lock if it exists
+      @wiki.unlock!(:document, current_user, :break => true )
     end
     @wiki.lock!(:document, current_user) if @wiki.document_open_for?(current_user)
     render :template => '/common/wiki/edit'
   end
 
-  #def cancel #test but did not work
-   # render :template => '/common/wiki/show'
-  #end
-
   def update
-    #begin
-    #if params[:break_lock]
-     # @wiki.unlock!(:document, current_user, :break => true ) #1st parameter is just a test
-     # render :partial => '/common/wiki/edit_area' #not ideal way to do this.
-     # return
-    #els
-    if !params[:cancel] #super hacky to have this if condition, but test for now
-      @wiki.update_document!(current_user, params[:wiki][:version], params[:wiki][:body])
-    else
+    if params[:cancel]
       @wiki.unlock!(:document, current_user, :break => true ) if @wiki
+    else
+      @wiki.update_document!(current_user, params[:wiki][:version], params[:wiki][:body])
     end
-    #unlock_for_current_user
-    #rescue Exception => exc
-    #  @message = exc.to_s
-    #  return render :partial => 'common/wiki/error'#(:action => 'error') # TODO: this should not be an action
-    #end
-    #redirect_to :action => :show
-    #render :template => '/common/wiki/show' #redirect doesn't work on firefox 3.6.23 (and maybe others)
   end
 
   protected
