2cf7a6dacd3c8afbf2fec3841203f174dcde153d
Azul <azul@riseup.net>
2011-11-22 11:34:10 +0100

    need to check for public_wiki not for the profiles

--- app/controllers/groups/home_controller.rb
+++ app/controllers/groups/home_controller.rb
@@ -19,17 +19,18 @@ class Groups::HomeController < Groups::BaseController
 
   def fetch_wikis
     if current_user.member_of? @group
-      @private_wiki = @group.profiles.private.try.wiki
-      @public_wiki = @group.profiles.public.try.wiki
+      @private_wiki = @group.profiles.private.wiki
+      @public_wiki = @group.profiles.public.wiki
       @wiki = just_edited_public_wiki? ? @public_wiki : @private_wiki
       # should we also show a particular wiki if we just viewed the versions of
       # that wiki? seems reasonable
     else
-      @wiki = @group.profiles.public.try.wiki
+      @wiki = @group.profiles.public.wiki
     end
   end
 
   def just_edited_public_wiki?
+    @public_wiki and
     request.referer and
     request.referer == edit_group_wiki_url(@group, @public_wiki)
   end
