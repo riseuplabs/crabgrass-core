3e5fc7f91db73661f79acf196f1a92c0835aaf30
Azul <azul@riseup.net>
2011-11-23 12:36:03 +0100

    cleaned up Common::Wiki controller module

--- app/controllers/common/wiki.rb
+++ app/controllers/common/wiki.rb
@@ -5,8 +5,6 @@
  This is the controller for the in-place wiki editor, not for the
  the wiki page type (wiki_page_controller.rb).
 
- Everything here is entirely ajax, for now.
-
 =end
 module Common::Wiki
 
@@ -44,8 +42,9 @@ module Common::Wiki
       @wiki.unlock!(:document, current_user, :break => true ) if @wiki
     else
       @wiki.update_document!(current_user, params[:wiki][:version], params[:wiki][:body])
+      success
     end
-    redirect_to entity_path(@group || @page)  #entity_path probably won't work for @page. should we use group_path for group?
+    redirect_to @page ? page_url(@page) : entity_path(@group)
 
   rescue Wiki::VersionExistsError, Wiki::SectionLockedOnSaveError => exc
     warning exc
@@ -58,6 +57,8 @@ module Common::Wiki
 
   protected
 
+  # I'm not sure we still need this. I wonder if we could include it in the
+  # wiki model as we know the context from the wikis profile / page owner.
   def setup_wiki_rendering
     return unless @wiki
     @wiki.render_body_html_proc {|body| render_wiki_html(body, @group.name)}
