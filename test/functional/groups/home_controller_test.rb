7c4ddba7936d1a3c086d350cf2fd3be90a4ceaa5
Azul <azul@riseup.net>
2011-11-24 12:06:57 +0100

    testing for the proper wikis being loaded on GRoups::HomeController

--- test/functional/groups/home_controller_test.rb
+++ test/functional/groups/home_controller_test.rb
@@ -6,6 +6,9 @@ class Groups::HomeControllerTest < ActionController::TestCase
     @user = User.make
     @group = Group.make
     @group.add_user!(@user)
+    @pub = @group.profiles.public.create_wiki
+    @priv = @group.profiles.private.create_wiki
+    Group.stubs(:find_by_name).with(@group.to_param).returns(@group)
   end
 
   def test_show
@@ -14,6 +17,21 @@ class Groups::HomeControllerTest < ActionController::TestCase
       get :show, :id => @group.to_param
     end
     assert_response :success
+    assert_equal @pub, assigns('public_wiki')
+    assert_equal @priv, assigns('private_wiki')
+    assert_equal @priv, assigns('wiki')
+  end
+
+  def test_show_after_editing_public
+    login_as @user
+    @request.env['HTTP_REFERER'] = edit_group_wiki_url(@group, @pub)
+    assert_permission :may_show_group_home? do
+      get :show, :id => @group.to_param
+    end
+    assert_response :success
+    assert_equal @pub, assigns('public_wiki')
+    assert_equal @priv, assigns('private_wiki')
+    assert_equal @pub, assigns('wiki')
   end
 
   def test_show_public
@@ -22,6 +40,9 @@ class Groups::HomeControllerTest < ActionController::TestCase
       get :show, :id => @group.to_param
     end
     assert_response :success
+    assert_equal @pub, assigns('wiki')
+    assert_nil assigns('private_wiki')
+    assert_nil assigns('public_wiki')
   end
 
   def test_may_not_show
