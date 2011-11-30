0c7a925a927270538d89b1349646d46310403370
Azul <azul@riseup.net>
2011-11-30 12:13:55 +0100

    using split panel now for versions view

--- app/helpers/wikis/versions_helper.rb
+++ app/helpers/wikis/versions_helper.rb
@@ -1,40 +1,18 @@
 module Wikis::VersionsHelper
 
-  def classes_for_versions_list(version)
-    version == @version ?
-      cycle('odd', 'even') + ' active' :
-      cycle('odd', 'even')
-  end
-
-  def previous_version_link
-    link_to_version LARROW + :pagination_previous.t, @version.previous
-  end
-
-  def next_version_link
-    link_to_version :pagination_next.t + RARROW, @version.next
-  end
-
-  def version_number_link(version)
-    link_to_version version.version, version
-  end
-
-  def version_time_link(version)
-    link_to_version friendly_date(version.updated_at), version
-  end
-
-  def link_to_version(content, version)
-    return unless version
-    link_to_remote content, :url => wiki_version_path(@wiki, version),
-      :method => :get
+  def short_description(version)
+    "Version " + version.version.to_s +
+      " created by " +
+      version_user_link(version)
   end
 
   def version_user_link(version)
-    link_to_user(version.user, :avatar => :xsmall) if version.user
+    if version.user
+      link_to_user(version.user)
+    else
+      ''
+    end
   end
-
- def version_user_link_small(version)
-   link_to avatar_for(version.user, :xsmall, {:title => version.user.name}), entity_path(version.user) if version.user
- end
 
   def version_action_links(version)
     link_line version_diff_link(version),
