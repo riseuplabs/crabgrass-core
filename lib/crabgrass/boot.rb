e44bbfdb29d3983d4bb6e89bcbcb56446bb3f2ab
jessib <jessib@riseup.net>
2012-01-24 11:37:35 -0800

    Updated boot order so we have active_support earlier, which is necessary to define methods like active_method_chain.

--- lib/crabgrass/boot.rb
+++ lib/crabgrass/boot.rb
@@ -1,7 +1,11 @@
 module Crabgrass
 end
 
+# do this early because environments/*.rb need it, as do some of the files in lib/extends
+require File.dirname(__FILE__) + '/conf'
+
 # load our core extends early, since they might be use anywhere.
+# active_support needs to be required before this, so we get methods like alias_method_chain
 Dir.glob("#{RAILS_ROOT}/lib/extends/*.rb").each do |file|
   require file
 end
@@ -11,9 +15,6 @@ require "#{CRABGRASS_PLUGINS_DIRECTORY}/crabgrass_mods/rails/boot"
 
 # load Crabgrass::Initializer early, it is used in environment.rb
 require File.dirname(__FILE__) + '/initializer'
-
-# do this early because environments/*.rb need it
-require File.dirname(__FILE__) + '/conf'
 
 # load configuration file
 Conf.load("crabgrass.#{RAILS_ENV}.yml")
