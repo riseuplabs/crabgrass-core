20aea097a99b87e3eeff340ace0ec82cc9078a2e
Jessi and Elijah <crabgrass@riseuplabs.org>
2012-02-10 15:51:51 -0800

    i18n: dummystrings.rb.. this is hackish, but it lets us figure out what strings are really used.

---
+++ lib/dummystrings.rb
@@ -0,0 +1,43 @@
+#
+# here lies translation keys that are important but do not actually appear anywhere in the code.
+#
+# for example: I18n.t @request.class.name.underscore
+#
+
+# requests:
+
+:request_to_destroy_our_group.t
+:request_to_friend.t
+:request_to_join_our_network.t
+:request_to_join_us.t
+:request_to_join_us_via_email.t
+:request_to_join_you.t
+:request_to_join_your_network.t
+:request_to_remove_user.t
+:votable_request.t
+
+# groups: 
+
+:group_description.t
+:committee_description.t
+:council_description.t
+:network_description.t
+
+# permissions:
+# appear as:
+# may_#{lock}_label
+# may_#{lock}_description
+
+:may_request_membership_description.t
+:may_view_description.t
+:may_see_contacts_description.t
+:may_see_groups_description.t
+:may_pester_description.t
+:may_request_contact_description.t
+
+:may_request_membership_label.t
+:may_view_label.t
+:may_see_contacts_label.t
+:may_see_groups_label.t
+:may_pester_label.t
+:may_request_contact_label.t
