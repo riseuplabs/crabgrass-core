ed5d2217d5ba427fc6bb7b1f943ae66cd5fb0fd2
Azul <azul@riseup.net>
2012-04-26 19:51:05 +0200

    updated schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead of editing this file,
+# This file is auto-generated from the current state of the database. Instead of editing this file, 
 # please use the migrations feature of Active Record to incrementally modify your database, and
 # then regenerate this schema definition.
 #
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110714001628) do
+ActiveRecord::Schema.define(:version => 20120406223541) do
 
   create_table "activities", :force => true do |t|
     t.integer  "subject_id"
@@ -84,26 +84,6 @@ ActiveRecord::Schema.define(:version => 20110714001628) do
     t.boolean "public",                                :default => false
   end
 
-  create_table "bdrb_job_queues", :force => true do |t|
-    t.text     "args",           :limit => 2147483647
-    t.string   "worker_name"
-    t.string   "worker_method"
-    t.string   "job_key"
-    t.integer  "taken"
-    t.integer  "finished"
-    t.integer  "timeout"
-    t.integer  "priority"
-    t.datetime "submitted_at"
-    t.datetime "started_at"
-    t.datetime "finished_at"
-    t.datetime "archived_at"
-    t.string   "tag"
-    t.string   "submitter_info"
-    t.string   "runner_info"
-    t.string   "worker_key"
-    t.datetime "scheduled_at"
-  end
-
   create_table "categories", :force => true do |t|
   end
 
@@ -166,6 +146,21 @@ ActiveRecord::Schema.define(:version => 20110714001628) do
   end
 
   add_index "dailies", ["page_id"], :name => "index_dailies_on_page_id"
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], :name => "delayed_jobs_priority"
 
   create_table "discussions", :force => true do |t|
     t.integer  "posts_count",      :default => 0
