b419e4d83643362bf3ffc3af504226a82118c61c
elijah <elijah@riseup.net>
2012-04-28 14:45:25 -0700

    user ghost - cleaned up typo and tab problems

--- app/models/user_ghost.rb
+++ app/models/user_ghost.rb
@@ -2,10 +2,10 @@ class UserGhost < User
 
   def name
     if read_attribute(:display_name).nil?
-	  :anonymous.t
-	else
-	  read_attribute(:display_name)
-	end
+      :anonymous.t
+    else
+      read_attribute(:display_name)
+    end
   end
   alias :login :name
   alias :display_name :login
@@ -23,12 +23,12 @@ class UserGhost < User
     profiles.destroy
     setting.destroy
     task_participations.destroy
-	participations.destroy
+    participations.destroy
     memberships.destroy
-	relationship.destroy
+    relationship.destroy
     clear_cache
   end
-  handle_asynchronously :retire
+  handle_asynchronously :retire!
 
   #
   # gets rid of the users name
@@ -36,31 +36,30 @@ class UserGhost < User
   def anonymize!
     self.update_attribute(:display_name, nil)
   end
-  handle_asynchronously :anonymize
+  handle_asynchronously :anonymize!
 
   #
   # gets rid of all comments
   #
   def destroy_comments!
-    self.posts.destroy    
+    self.posts.destroy
   end
-  handle_asynchronously :destroy_comments  
+  handle_asynchronously :destroy_comments!
 
   private
-  
+
   def clean_attributes
     #
-	# use update_attribute to bypass the validations
-	#
-	attrs_to_nil_out = %q(
-	  crypted_password salt time_zone email
-	  login created_at updated_at version
-	  last_seen_at language remember_token remember_token_expires_at
-   	)
-	attrs_to_nil_out.each do |attr|
-	  update_attribute(attr, nil)
-	end
+    # use update_attribute to bypass the validations
+    #
+    attrs_to_nil_out = %q(
+      crypted_password salt time_zone email
+      login created_at updated_at version
+      last_seen_at language remember_token remember_token_expires_at
+    )
+    attrs_to_nil_out.each do |attr|
+      update_attribute(attr, nil)
+    end
   end
-  
-  
+
 end
