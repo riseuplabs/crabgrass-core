88668e10e951777b2fbe5e2340a01c50b63b0d28
Azul <azul@riseup.net>
2012-05-16 09:39:51 +0200

    major upload.js refactoring - no more globals

--- app/assets/javascripts/upload.js
+++ app/assets/javascripts/upload.js
@@ -1,206 +1,254 @@
-var pendingFiles = [];
-var uploading = false;
+// let's not clutter the global scope
+(function() {
 
-// Function that will allow us to know if Ajax uploads are supported
-function supportAjaxUploadWithProgress() {
-  return supportFileAPI() && supportAjaxUploadProgressEvents() && supportFormData();
-
-  // Is the File API supported?
-  function supportFileAPI() {
-    var fi = document.createElement('INPUT');
-    fi.type = 'file';
-    return 'files' in fi;
-  };
-
-  // Are progress events supported?
-  function supportAjaxUploadProgressEvents() {
-    var xhr = new XMLHttpRequest();
-    return !! (xhr && ('upload' in xhr) && ('onprogress' in xhr.upload));
-  };
-
-  // Is FormData supported?
-  function supportFormData() {
-    return !! window.FormData;
-  }
-}
-
-// Actually confirm support
-
-document.observe("dom:loaded", function() {
-  if (supportAjaxUploadWithProgress()) {
-    // Ajax uploads are supported!
-    // Init the single-field file upload
-    initFileOnlyAjaxUpload();
-  }
-});
-
-function initFileOnlyAjaxUpload() {
-  var fileInput = document.getElementById('file-id');
-  var fileDrop = document.getElementById('file-drop');
-  if (fileInput) fileInput.onchange = onFileSelected;
-  if (fileDrop) {
-    fileDrop.addEventListener("drop", onFileDropped, true);
-    fileDrop.addEventListener("dragenter", onDragEnter, true);
-    fileDrop.addEventListener("dragover", onDragOver, true);
-    fileDrop.addEventListener("dragleave", onDragLeave, true);
-  }
-}
-
-function onDragEnter(e) {
-  e.currentTarget.classList.add("dragging");
-  e.stopPropagation();  
-  e.preventDefault(); 
-}
-
-function onDragOver(e) {
-  e.stopPropagation();  
-  e.preventDefault(); 
-}
-
-function onDragLeave(e) {
-  e.currentTarget.classList.remove("dragging");
-  e.stopPropagation();  
-  e.preventDefault(); 
-}
-
-function onFileSelected(e) {
-  // FormData only has the file
-  var fileInput = document.getElementById('file-id');
-  addFiles(fileInput.files);
-}
-
-function onFileDropped(e) {
-  addFiles(e.dataTransfer.files);
-  e.stopPropagation();  
-  e.preventDefault(); 
-}
-
-function addFiles(newFiles) {
-  for (var i = 0; i < newFiles.length; i += 1) {
-    pendingFiles.push(newFiles[i]);
-  }
-  if(!uploading) startUpload();
-}
-
-function startUpload() {
-  var form = document.getElementById('form-id');
-  var action = form.action;
-  uploading = true;
-  var current;
-  // TODO: we already have a request queue somewhere - combine!
-  whilst(getNextFile, uploadFile, done);
-
-  function getNextFile() {
-    return current = pendingFiles.shift();
-  }
-
-  function uploadFile(callback) {
-    onProcessingFile(current);
-    // Since this is the file only, we send it to a specific location
-    var formData = new FormData(form);
-    formData.append('asset[uploaded_data]', current);
-    // Code common to both variants
-    sendXHRequest(formData, action, callback);
-  }
-
-  function done() {
-    uploading = false
-    var upload = document.getElementById('current-upload');
-    upload.setAttribute("class", "hidden")
-  };
-}
-
-// Once the FormData instance is ready and we know
-// where to send the data, the code is the same
-// for both variants of this technique
-function sendXHRequest(formData, uri, callback) {
-  // Get an XMLHttpRequest instance
-  var xhr = new XMLHttpRequest();
-
-  // Set up events
-  xhr.upload.addEventListener('loadstart', onloadstartHandler, false);
-  xhr.upload.addEventListener('progress', onprogressHandler, false);
-  xhr.upload.addEventListener('load', onloadHandler, false);
-  xhr.upload.addEventListener('load', function(evt) { callback() }, false);
-  xhr.addEventListener('readystatechange', onreadystatechangeHandler, false);
-
-  // Set up request
-  xhr.open('POST', uri, true);
-  // make rails recognize this as xhr
-  xhr.setRequestHeader('X-Requested-With','XMLHttpRequest')
-
-  // Fire!
-  xhr.send(formData);
-}
-
-function showUploadState() {
-  var upload = document.getElementById('current-upload');
-  upload.setAttribute("class", "alert hook")
-}
-
-function updateUploadState(message, filename) {
-  if (message) document.getElementById('upload-message').innerHTML = message;
-  if (filename) document.getElementById('upload-filename').innerHTML = filename;
-}
-
-function onProcessingFile(file) {
-  showUploadState();
-  updateUploadState("starting upload...", file.name);
-}
-
-// Handle the start of the transmission
-function onloadstartHandler(evt) {
-  updateUploadState("upload started ...");
-}
-
-// Handle the end of the transmission
-function onloadHandler(evt) {
-  var progress = document.getElementById('upload-progress');
-  // TODO: make sure this is not hidden behind progress bar
-  updateUploadState("upload successful ...");
-  progress.innerHTML = '&nbsp;100&nbsp;%';
-  progress.setAttribute("style", "width: 100%");
-}
-
-// Handle the progress
-function onprogressHandler(evt) {
-  var percent = Math.round(evt.loaded/evt.total*100);
-  var progress = document.getElementById('upload-progress');
-  progress.innerHTML = '&nbsp;' + percent + '&nbsp;%';
-  progress.setAttribute("style", "width: " + percent + '%;');
-}
-
-// Handle the response from the server
-function onreadystatechangeHandler(evt) {
-  if (evt.target.readyState != 4) return;
-  var status = null;
-
-  try {
-    status = evt.target.status;
-  }
-  catch(e) {
-    return;
-  }
-
-  if (status == '200' && evt.target.responseText) {
-    // TODO: is this the way to go?
-    var resp = evt.target.responseText;
-    eval(resp);
-  }
-}
-
-// taken from the brilliant async.js
-function whilst(test, iterator, callback) {
-  if (test()) {
-    iterator(function (err) {
-      if (err) {
-        return callback(err);
+  var uploadList = {
+    // view callback
+    changed: function(){},
+    pendingFiles: [],
+    uploading: false,
+    addFiles: function(newFiles) {
+      for (var i = 0; i < newFiles.length; i += 1) {
+        this.pendingFiles.push(newFiles[i]);
       }
-      whilst(test, iterator, callback);
-    });
-  }
-  else {
-    callback();
-  }
-};
+      if(!this.uploading) this.startUpload();
+    },
+    startUpload: function() {
+      this.uploading = true;
+      // TODO: we already have a request queue somewhere - combine!
+      whilst(getNextFile, uploadFile.start, done);
 
+      function getNextFile() {
+        return uploadFile.setFile(uploadList.pendingFiles.shift());
+      }
+
+      function done() {
+        uploadList.uploading = false
+      };
+    }
+  }
+
+  var uploadFile = (function(){
+    var file, action, percent, message, form, processing;
+
+    function init(_form) {
+      action = _form.action;
+      form = _form;
+    }
+
+    function start(callback) {
+      // Since this is the file only, we send it to a specific location
+      var formData = new FormData(form);
+      formData.append('asset[uploaded_data]', file);
+      // Code common to both variants
+      sendXHRequest(formData, callback);
+      uploadFile.changed();
+    }
+
+    function sendXHRequest(formData, callback) {
+      // Get an XMLHttpRequest instance
+      var xhr = new XMLHttpRequest();
+
+      // Set up events
+      xhr.upload.addEventListener('loadstart', onloadstartHandler, false);
+      xhr.upload.addEventListener('progress', onprogressHandler, false);
+      xhr.upload.addEventListener('load', onloadHandler, false);
+      xhr.upload.addEventListener('load', function(evt) { callback() }, false);
+      xhr.addEventListener('readystatechange', onreadystatechangeHandler, false);
+
+      // Set up request
+      xhr.open('POST', action, true);
+      // make rails recognize this as xhr
+      xhr.setRequestHeader('X-Requested-With','XMLHttpRequest')
+
+      // Fire!
+      xhr.send(formData);
+      
+      // Handle the start of the transmission
+      function onloadstartHandler(evt) {
+        message = "upload started ...";
+        uploadFile.changed();
+      }
+
+      // Handle the end of the transmission
+      function onloadHandler(evt) {
+        message = "processing upload ...";
+        percent = 100;
+        processing = true;
+        uploadFile.changed();
+      }
+
+      function onprogressHandler(evt) {
+        percent = Math.round(evt.loaded/evt.total*100);
+        uploadFile.changed();
+      }
+
+      // Handle the response from the server
+      function onreadystatechangeHandler(evt) {
+        if (evt.target.readyState != 4) return;
+        var status = null;
+
+        try {
+          status = evt.target.status;
+        }
+        catch(e) {
+          return;
+        }
+
+        if (status == '200' && evt.target.responseText) {
+          // TODO: is this the way to go?
+          processing = false;
+          uploadFile.changed();
+          var resp = evt.target.responseText;
+          eval(resp);
+        }
+      }
+
+    }
+    
+    return {
+      // callback for the view
+      changed: function() {},
+      // initialize with a form to get params from
+      init: init,
+      // trigger upload
+      start: start,
+      setFile: function(_file) { return file = _file },
+      getFile: function() { return file },
+      getPercent: function() { return percent },
+      getMessage: function() { return message },
+      isProcessing: function() { return !!processing }
+    }
+  }());
+
+  var uploadView = {
+    container: null,
+    init: function(_container){
+      this.container =_container;
+    },
+    render: function() {
+      if (!uploadList.uploading && !uploadFile.isProcessing()) {
+        this.hide();
+        return
+      }
+      var html = '<span id="upload-filename" class="left">#{filename} (#{left})</span>' +
+                 '<div class="progress progress-striped progress-active">' +
+                 '<div class="bar" style="width: #{percent}%;"></div>' +
+                 '</div>' +
+                 '<span class="left">#{message}</span>';
+      this.container.innerHTML = html.interpolate({
+        filename: uploadFile.getFile().name,
+        percent: uploadFile.getPercent(),
+        message: uploadFile.getMessage(),
+        left: uploadList.pendingFiles.length
+      });
+      this.container.classList.remove("hidden");
+    },
+    hide: function() {
+      if (uploadView.container.innerHTML != "") {
+        setTimeout(function () {
+          uploadView.container.classList.add("hidden")
+          uploadView.container.innerHTML = ""
+        },1000);
+      }
+    }
+  }
+
+
+  // Actually confirm support
+
+  document.observe("dom:loaded", function() {
+    if (supportAjaxUploadWithProgress()) {
+      // Ajax uploads are supported!
+      // Init the single-field file upload
+      initAjaxUpload();
+    }
+  });
+
+  function initAjaxUpload() {
+    var fileInput = document.getElementById('file-id');
+    var fileDrop = document.getElementById('file-drop');
+    var form = document.getElementById('form-id');
+    if (!form) return;
+    if (!fileInput && !fileDrop) return;
+    uploadFile.init(form);
+    if (fileInput) fileInput.onchange = onFileSelected;
+    if (fileDrop) {
+      fileDrop.addEventListener("drop", onFileDropped, true);
+      fileDrop.addEventListener("dragenter", onDragEnter, true);
+      fileDrop.addEventListener("dragover", onDragOver, true);
+      fileDrop.addEventListener("dragleave", onDragLeave, true);
+    }
+    uploadList.changed = uploadView.render.bind(uploadView);
+    uploadFile.changed = uploadView.render.bind(uploadView);
+    uploadView.init(document.getElementById('current-upload'));
+  }
+
+  function onDragEnter(e) {
+    e.currentTarget.classList.add("dragging");
+    e.stopPropagation();  
+    e.preventDefault(); 
+  }
+
+  function onDragOver(e) {
+    e.stopPropagation();  
+    e.preventDefault(); 
+  }
+
+  function onDragLeave(e) {
+    e.currentTarget.classList.remove("dragging");
+    e.stopPropagation();  
+    e.preventDefault(); 
+  }
+
+  function onFileSelected(e) {
+    // FormData only has the file
+    var fileInput = document.getElementById('file-id');
+    uploadList.addFiles(fileInput.files);
+  }
+
+  function onFileDropped(e) {
+    uploadList.addFiles(e.dataTransfer.files);
+    e.stopPropagation();  
+    e.preventDefault(); 
+  }
+
+  // HELPERS
+
+  // Function that will allow us to know if Ajax uploads are supported
+  function supportAjaxUploadWithProgress() {
+    return supportFileAPI() && supportAjaxUploadProgressEvents() && supportFormData();
+
+    // Is the File API supported?
+    function supportFileAPI() {
+      var fi = document.createElement('INPUT');
+      fi.type = 'file';
+      return 'files' in fi;
+    };
+
+    // Are progress events supported?
+    function supportAjaxUploadProgressEvents() {
+      var xhr = new XMLHttpRequest();
+      return !! (xhr && ('upload' in xhr) && ('onprogress' in xhr.upload));
+    };
+
+    // Is FormData supported?
+    function supportFormData() {
+      return !! window.FormData;
+    }
+  }
+
+  // taken from the brilliant async.js
+  function whilst(test, iterator, callback) {
+    if (test()) {
+      iterator(function (err) {
+        if (err) {
+          return callback(err);
+        }
+        whilst(test, iterator, callback);
+      });
+    }
+    else {
+      callback();
+    }
+  };
+}());
