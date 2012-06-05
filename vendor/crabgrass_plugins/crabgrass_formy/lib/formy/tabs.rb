e3da0f456c0c643e8a4bcd1a86f7ec50fb648023
Azul <azul@riseup.net>
2012-06-05 16:30:34 +0200

    added toggle bug version of tags - this is more flexible than the toggle_bug_links

--- vendor/crabgrass_plugins/crabgrass_formy/lib/formy/tabs.rb
+++ vendor/crabgrass_plugins/crabgrass_formy/lib/formy/tabs.rb
@@ -111,15 +111,25 @@ module Formy
 
     def open
       super
+      open_group
+    end
+
+    def close
+      close_group
+      super
+    end
+
+    protected
+
+    def open_group
       puts "<div style='height:1%'>" # this is to force hasLayout in ie
       puts "<ul class='nav nav-tabs #{@options[:class]}'>"
     end
 
-    def close
+    def close_group
       @elements.each {|e| raw_puts e}
       puts "<li></li></ul>"
       puts "</div>"
-      super
     end
 
   end
