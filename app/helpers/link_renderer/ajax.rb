0ed09ea2047ebba5e859702ba3645e5b3b38a128
Azul <azul@riseup.net>
2012-06-09 15:39:34 +0200

    fixed pagination when inserting images into a wiki

--- app/helpers/link_renderer/ajax.rb
+++ app/helpers/link_renderer/ajax.rb
@@ -4,18 +4,11 @@
 #
 #
 class LinkRenderer::Ajax < LinkRenderer::CrabgrassBase
-  
+
   def page_link_to(page, text, attributes = {})
-    # ajax pagination will always use :get as the method
-    # because the action should be index (or possibly show)
-    options = {
-      :url => url_for(page),
-      :method => :get,
-      :loading => @template.show_spinner(spinner_id)
-    }
-    @template.link_to_remote(text, options, attributes)
+    @template.link_to_remote(text, link_options(page), attributes)
   end
-  
+
   #def html_after
   #  @template.spinner(spinner_id)
   #end
@@ -24,6 +17,19 @@ class LinkRenderer::Ajax < LinkRenderer::CrabgrassBase
     # eg, if we are paginating user_participations, results in spinners with
     # id => 'pagination_user_participation_spinner'
     'pagination_' + @collection.first.class.name.underscore
+  end
+
+  protected
+
+  # overwritten by LinkRenderer::ModalAjax
+  def link_options(page)
+    # ajax pagination will always use :get as the method
+    # because the action should be index (or possibly show)
+    options = {
+      :url => url_for(page),
+      :method => :get,
+      :loading => @template.show_spinner(spinner_id)
+    }
   end
 
 end
