0e542f268b3e4967340634a8c312be0fad25f772
Azul <azul@riseup.net>
2012-07-30 16:40:48 +0200

    abort if user canceled group wiki creation - fixes #4011

--- app/controllers/groups/wikis_controller.rb
+++ app/controllers/groups/wikis_controller.rb
@@ -5,6 +5,8 @@ class Groups::WikisController < Groups::BaseController
   permission_helper 'wikis'
   helper 'wikis/base'
 
+  before_filter :abort_on_cancel, :only => :create
+
   def new
     if @wiki = @profile.wiki
       # the wiki has been created by another user since the link to
@@ -17,18 +19,16 @@ class Groups::WikisController < Groups::BaseController
   end
 
   def create
-    if !params[:cancel]
-      if @wiki = @profile.wiki
-        # another user has created this group wiki
-        # we will save this one as a newer version
-        @wiki.update_document!(current_user, nil, params[:wiki][:body])
-        notice :wiki_existed_new_version_created.t
-      else
-        @wiki = @profile.create_wiki :version => 0,
-          :body => params[:wiki][:body],
-          :user => current_user
-        success
-      end
+    if @wiki = @profile.wiki
+      # another user has created this group wiki
+      # we will save this one as a newer version
+      @wiki.update_document!(current_user, nil, params[:wiki][:body])
+      notice :wiki_existed_new_version_created.t
+    else
+      @wiki = @profile.create_wiki :version => 0,
+        :body => params[:wiki][:body],
+        :user => current_user
+      success
     end
     redirect_to group_home_path(@group, :wiki_id => @wiki.id)
   end
@@ -53,4 +53,10 @@ class Groups::WikisController < Groups::BaseController
     @wiki = @group.wikis.find(params[:id]) # this could be nil
   end
 
+  def abort_on_cancel
+    if params[:cancel]
+      redirect_to group_home_path(@group)
+      return
+    end
+  end
 end
