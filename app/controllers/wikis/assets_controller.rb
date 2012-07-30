fbce1b449cb59ddb40c5d45441d6ee52a8f647e7
Azul <azul@riseup.net>
2012-07-30 15:22:14 +0200

    actually attach the asset to the page if we have one

--- app/controllers/wikis/assets_controller.rb
+++ app/controllers/wikis/assets_controller.rb
@@ -7,7 +7,7 @@ class Wikis::AssetsController < Wikis::BaseController
 
   # response goes to an iframe, so requires responds_to_parent
   def create
-    asset = Asset.build :uploaded_data => params[:asset][:uploaded_data]
+    asset = Asset.build :uploaded_data => params[:asset][:uploaded_data], :parent_page => @page
     @page ||= asset.create_page(current_user, @wiki.context)
     asset.save
     fetch_assets # now the new one should be included
