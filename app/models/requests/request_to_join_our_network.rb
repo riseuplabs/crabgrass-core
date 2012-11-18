ee98171b01ee1a2ed406e1fd200fe895adb7848c
Azul <azul@riseup.net>
2012-11-18 12:28:22 +0100

    validations to prevent networks from joining networks
    Also blocking committees of Networks from joining other networks.
    
    Why? Because we need to prevent endless loops.
    
    These are very uncommon usecases anyway and there are lot's of
    ways to work around them - namely making one network a group or making
    one network public and open and linking it from the other one.

--- app/models/requests/request_to_join_our_network.rb
+++ app/models/requests/request_to_join_our_network.rb
@@ -8,17 +8,11 @@ class RequestToJoinOurNetwork < Request
   validates_format_of :requestable_type, :with => /Group/
   validates_format_of :recipient_type, :with => /Group/
 
-  def validate_on_create
-    unless requestable.type =~ /Network/
-      errors.add_to_base('requestable must be a network')
-    end
-    if Federating.find_by_group_id_and_network_id(group.id, network.id)
-      errors.add_to_base(I18n.t(:membership_exists_error, :member => group.name))
-    end
-    if RequestToJoinOurNetwork.having_state(state).find_by_recipient_id_and_requestable_id_and_state(recipient_id, requestable_id, state)
-      errors.add_to_base(I18n.t(:request_exists_error, :recipient => recipient.name))
-    end
-  end
+  validate :requestable_is_network
+  validate :no_membership_yet
+  validate :no_request_yet
+  validate :group_is_not_network
+  validate :group_is_not_network_committee
 
   def network() requestable end
   def group() recipient end
@@ -54,6 +48,37 @@ class RequestToJoinOurNetwork < Request
   def icon_entity
     self.recipient
   end
-  
+
+  protected
+
+  def requestable_is_network
+    unless requestable.type =~ /Network/
+      errors.add_to_base('requestable must be a network')
+    end
+  end
+
+  def no_membership_yet
+    if Federating.find_by_group_id_and_network_id(group.id, network.id)
+      errors.add_to_base(I18n.t(:membership_exists_error, :member => group.name))
+    end
+  end
+
+  def no_request_yet
+    if RequestToJoinOurNetwork.having_state(state).find_by_recipient_id_and_requestable_id_and_state(recipient_id, requestable_id, state)
+      errors.add_to_base(I18n.t(:request_exists_error, :recipient => recipient.name))
+    end
+  end
+
+  def group_is_not_network
+    if group.network?
+      errors.add_to_base(I18n.t(:networks_may_not_join_networks))
+    end
+  end
+
+  def group_is_not_network_committee
+    if group.committee? && group.parent.network?
+      errors.add_to_base(I18n.t(:network_committees_may_not_join_networks))
+    end
+  end
 end
 
