cc487da00ea37da94a944c941e096cdd7b1b40fc
() <nilclass@riseup.net>
2013-02-12 14:58:15 +0100

    removed filter_parameter_logging from ApplicationController (config.filter_parameters already set in config/application.rb)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@
 class ApplicationController < ActionController::Base
 
   prepend_view_path "app/common/views"
-  filter_parameter_logging :password
   protect_from_forgery
   layout proc{ |c| c.request.xhr? ? false : 'application' } # skip layout for ajax
 
