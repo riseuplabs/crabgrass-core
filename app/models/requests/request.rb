71b42c1fec4814dd65e7f32862466330de590ec4
() <nilclass@riseup.net>
2013-02-12 14:33:09 +0100

    deprecations: * validate_on_create(:x) -> validate(:x, :on => :create) * before_validations_on_create(:y) -> before_validation(:y, :on => :create)

--- app/models/requests/request.rb
+++ app/models/requests/request.rb
@@ -58,10 +58,10 @@ class Request < ActiveRecord::Base
   validates_presence_of :recipient_id,   :if => :recipient_required?
   validates_presence_of :requestable_id, :if => :requestable_required?
 
-  validate_on_create :no_duplicate
-  validate_on_create :check_create_permission
+  validate :no_duplicate, :on => :create
+  validate :check_create_permission, :on => :create
 
-  before_validation_on_create :set_default_state
+  before_validation :set_default_state, :on => :create
 
   ##
   ## FINDERS
