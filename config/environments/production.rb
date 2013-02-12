09eefc95621d1280aadbeecafb762402f9b59bc6
() <nilclass@riseup.net>
2013-02-12 10:52:00 +0100

    commented in deprecated config options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Crabgrass::Application.configure do
   config.cache_classes = true
   config.action_controller.consider_all_requests_local = true
   config.action_controller.perform_caching             = true
-  config.action_view.cache_template_loading            = true
+  #config.action_view.cache_template_loading            = true
 
   ##
   ## LOGGING
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,7 +18,7 @@ Crabgrass::Application.configure do
   config.whiny_nils = true
   config.action_controller.consider_all_requests_local = true
   config.action_controller.perform_caching             = false
-  config.action_view.cache_template_loading            = true
+  #config.action_view.cache_template_loading            = true
   config.action_controller.allow_forgery_protection    = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :test
