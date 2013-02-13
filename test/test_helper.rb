cce35456f3b38c4213d7811120783850368140ee
Azul <azul@riseup.net>
2013-02-13 16:58:22 +0100

    rails 3 expects require 'rails/test_help'

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,7 +9,7 @@ require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 if defined?(UNIT_TESTING)
   require File.expand_path(File.dirname(__FILE__) + "/unit/test_help")
 else
-  require 'test_help'
+  require 'rails/test_help'
 end
 
 ##
