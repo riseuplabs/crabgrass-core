227552b20f31886f10c7405ee4b666afb82e6ef2
Azul <azul@riseup.net>
2013-02-13 15:09:55 +0100

    using the new #maximum shortcut

--- vendor/plugins/acts_as_versioned/lib/acts_as_versioned.rb
+++ vendor/plugins/acts_as_versioned/lib/acts_as_versioned.rb
@@ -392,7 +392,7 @@ module ActiveRecord #:nodoc:
 
           # Gets the next available version for the current record, or 1 for a new record
           def next_version
-            (new_record? ? 0 : versions.calculate(:max, :version).to_i) + 1
+            (new_record? ? 0 : versions.maximum(:version).to_i) + 1
           end
 
         module ClassMethods
