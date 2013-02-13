3450bdc03aad5cdb699d9a188b0563cb2c1a347d
() <nilclass@riseup.net>
2013-02-13 12:50:25 +0100

    another attempt to fix BIT_OR for arel

--- vendor/crabgrass_plugins/castle_gates/lib/castle_gates/arel_extension.rb
+++ vendor/crabgrass_plugins/castle_gates/lib/castle_gates/arel_extension.rb
@@ -5,9 +5,23 @@ module Arel
     end
   end
 
-  module Attribute::Expressions
+  module Expressions
     def bit_or
-      Nodes::BitOr.new(self)
+      Nodes::BitOr.new([self], Nodes::SqlLiteral.new('bit_or_id'))
+    end
+  end
+
+  module Visitors
+    class DepthFirst < Arel::Visitors::Visitor
+      alias :visit_Arel_Nodes_BitOr :function
+    end
+
+    class ToSql < Arel::Visitors::Visitor
+      def visit_Arel_Nodes_BitOr o
+        expressions = o.expressions.map { |x| visit x }.join(', ')
+        aliaz = o.alias ? " AS #{visit o.alias}" : ''
+        "BIT_OR(#{expressions})#{aliaz}"
+      end
     end
   end
 end
