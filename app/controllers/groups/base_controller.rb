f4c789890a148e2aced6b9e4a045a1f0b33d0509
Azul <azul@riseup.net>
2013-02-13 16:57:25 +0100

    removed group_avatar_helpers - we should use network.becomes(Group) if needed

--- app/controllers/groups/base_controller.rb
+++ app/controllers/groups/base_controller.rb
@@ -22,25 +22,6 @@ class Groups::BaseController < ApplicationController
     super
   end
 
-  ##
-  ## PATHS
-  ##
-
-  # sometimes it is nice to rely on the way rails will guess resource
-  # routes based on the class. so, we alias some of the group routes to be
-  # also supported by networks, councils, and committees.
-  def self.path_alias(path_method)
-    path_method = path_method.to_s
-    for type in ['network', 'committee', 'council']
-      new_method = path_method.sub(/^group/, type)
-      alias_method new_method, path_method
-      helper_method new_method
-    end
-  end
-
-  path_alias :group_avatars_path
-  path_alias :group_avatar_path
-
   def new_group_committee_path(group)
     new_group_structure_path(group, :type => 'committee')
   end
