667a77ec24f5064eff729aa07e0e1de6e582a5a7
jessi <jessib@riseup.net>
2013-02-15 16:50:06 -0800

    Another tweak to avoid errors if version is nil, but not sure that version should ever be nil

--- app/models/user_extension/cache.rb
+++ app/models/user_extension/cache.rb
@@ -127,7 +127,7 @@ module UserExtension
     # or directly when a new contact is added
     def update_contacts_cache()
       friend,foe = get_contact_ids
-      update_attributes :version => version+1, # might version be nil?
+      update_attributes :version => (version||-1) +1, # this fixes if version is nil, but probably we should get at the root of that.
         :friend_id_cache => friend,
         :foe_id_cache    => foe
     end
