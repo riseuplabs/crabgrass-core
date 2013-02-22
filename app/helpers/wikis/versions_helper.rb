0dc1d2cf90c4578d6ba234775f56a7e6fe10899c
() <nilclass@riseup.net>
2013-02-22 16:17:29 +0100

    use ajax request for "revert" and "destroy" links in wiki versions, so the layout doesn't get busted.

--- app/helpers/wikis/versions_helper.rb
+++ app/helpers/wikis/versions_helper.rb
@@ -42,12 +42,12 @@ module Wikis::VersionsHelper
     return unless may_revert_wiki_version?(version)
     link_to :wiki_version_revert_link.t,
       revert_wiki_version_path(@wiki, version),
-      :method => :post
+      :method => :post, :remote => true
   end
 
   def version_delete_link(version)
     return unless may_admin_wiki?
     link_to :wiki_version_destroy_link.t,
-      wiki_version_path(@wiki, version), :method => :delete
+      wiki_version_path(@wiki, version), :method => :delete, :remote => true
   end
 end
