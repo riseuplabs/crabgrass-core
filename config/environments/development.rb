88c2378770fe343bb00da8b4c430520982ccda09
Pietro Monteiro <pietro@deckmonitoring.com>
2013-03-06 18:35:42 -0800

    debug_rjs was removed.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,7 +20,7 @@ Crabgrass::Application.configure do
   config.cache_classes = false
   config.whiny_nils = true
   config.consider_all_requests_local = true
-  config.action_view.debug_rjs                         = true
+  #config.action_view.debug_rjs                         = true
   config.action_controller.perform_caching             = false
   config.action_mailer.raise_delivery_errors = false
   config.log_level = :debug
