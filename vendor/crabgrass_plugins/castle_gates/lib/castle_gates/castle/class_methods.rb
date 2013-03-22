a10517b8e389faf3677a42551b7c2eafcc383e2a
Azul <azul@riseup.net>
2013-03-22 10:40:43 +0100

    allow grant_access :public => [] to work
    This should obviously be a noop but it's very helpful for migration scripts not always to test if there are permissions

--- vendor/crabgrass_plugins/castle_gates/lib/castle_gates/castle/class_methods.rb
+++ vendor/crabgrass_plugins/castle_gates/lib/castle_gates/castle/class_methods.rb
@@ -12,10 +12,6 @@ module ClassMethods
   def conditions_for_gates(gate_names)
     gate_names = [gate_names] unless gate_names.is_a? Array
     bits = gate_set.bits(gate_names)
-    unless bits
-      raise ArgumentError.new 'bad gate names %s' % gate_names.inspect
-      exit
-    end
     "(#{bits} & ~keys.gate_bitfield) = 0"
   end
 
