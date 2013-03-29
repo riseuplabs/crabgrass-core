b7fb49de6d38511e397ee484ba2587d110150879
() <nilclass@riseup.net>
2013-03-29 10:20:57 +0100

    correctly display group summary HTML in groups directory

--- app/helpers/groups/directory_helper.rb
+++ app/helpers/groups/directory_helper.rb
@@ -16,7 +16,7 @@ module Groups::DirectoryHelper
     haml do
       haml '.name', link_to_group(group)
       haml '.info', comma_join(place, count)
-      haml '.summary.plain', summary
+      haml '.summary.plain', summary.try.html_safe
       if committees.present?
         haml '.committees' do
           for cmtee in committees
