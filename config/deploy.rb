88536a9bf5982471617bf52cd8d9310f30390d23
Azul <azul@riseup.net>
2013-04-29 15:33:44 +0200

    we need to cleanup the old data before running migrations

--- config/deploy.rb
+++ config/deploy.rb
@@ -218,4 +218,4 @@ before  "deploy:finalize_update", "crabgrass:compile_assets"
 after  "deploy:create_symlink", "crabgrass:create_version_files"
 after  "deploy:restart", "passenger:restart", "deploy:cleanup"
 
-before 'crabgrass:upgrade_to_0_9', 'crabgrass:cleanup_outdated_data', 'deploy:migrations'
+before 'crabgrass:upgrade_to_0_9', 'deploy', 'crabgrass:cleanup_outdated_data', 'deploy:migrate'
