24ddf79f34265635dbdcb62102c7eaac267948d1
() <nilclass@riseup.net>
2013-04-29 16:48:59 +0200

    use SQL to convert language codes to two-letter form
    using ruby takes ages and causes validation errors

--- db/migrate/20091101000109_convert_language_codes_to_two_letters.rb
+++ db/migrate/20091101000109_convert_language_codes_to_two_letters.rb
@@ -1,32 +1,9 @@
 class ConvertLanguageCodesToTwoLetters < ActiveRecord::Migration
   def self.up
-    Language.find(:all).each do |language|
-      language.code.gsub!(/_\w\w/, '')
-      language.save!
-    end
-
-    User.find(:all, :conditions => "language RLIKE '[a-z][a-z]_[A-Z][A-Z]'").each do |user|
-      user.language.gsub!(/_\w\w/, '')
-      begin
-        user.save!
-      rescue StandardError => err
-        if err =~ /Email is an invalid email/
-          user.update_attribute('email', '')
-          user.save!
-        else
-          puts "Could not save #{user.login}: #{err}"
-        end
-      end
-    end
-
-    Group.find(:all, :conditions => "language RLIKE '[a-z][a-z]_[A-Z][A-Z]'").each do |group|
-      group.language.gsub!(/_\w\w/, '')
-      begin
-        group.save!
-      rescue StandardError => err
-        puts "Could not save #{group.name}: #{err}"
-      end
-    end
+    connection = ActiveRecord::Base.connection
+    connection.execute(%Q{ UPDATE languages SET code = LEFT(languages.code, 2) }) if connection.table_exists? 'languages'
+    connection.execute(%Q{ UPDATE users SET language = LEFT(users.language, 2) WHERE language IS NOT NULL })
+    connection.execute(%Q{ UPDATE groups SET language = LEFT(groups.language, 2) WHERE language IS NOT NULL })
   end
 
   def self.down
