ab9f38e1897dbc05c458737a17881a7fdf002df0
Azul <azul@riseup.net>
2014-02-16 13:32:31 +0100

    fix search and remove order by login
    Order by login was making the queries crazy slow

--- app/controllers/people/directory_controller.rb
+++ app/controllers/people/directory_controller.rb
@@ -6,7 +6,7 @@ class People::DirectoryController < ApplicationController
 
   def index
     @users = finder.find_by_path(params[:path])
-    @users = @users.order(:login).paginate(pagination_params) if @users
+    @users = @users.paginate(pagination_params) if @users
   end
 
   protected
@@ -22,8 +22,8 @@ class People::DirectoryController < ApplicationController
   end
 
   def default_path
-    if !logged_in?
-      'search'
+    if !logged_in? || params[:q].present?
+      'search/' + params[:q]
     elsif current_user.friends.any?
       'contacts'
     elsif current_user.peers.any?
