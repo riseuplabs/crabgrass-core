dbc12487da7166229cd06adfa24a8ef3723821da
Azul <azul@riseup.net>
2014-09-07 11:59:17 +0200

    minor: tests: fix assertion for no errors

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -11,7 +11,7 @@ class UserTest < ActiveSupport::TestCase
   def test_user_fixtures_are_valid
     orange = users(:orange)
     orange.valid?
-    assert_equal Hash.new, orange.errors
+    assert_equal Hash.new, orange.errors.messages
     assert orange.valid?
   end
 
@@ -20,7 +20,7 @@ class UserTest < ActiveSupport::TestCase
     orange = users(:orange)
     orange.email = nil
     orange.valid?
-    assert_equal Hash.new, orange.errors
+    assert_equal Hash.new, orange.errors.messages
     assert orange.valid?
   end
 
