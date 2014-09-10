6a706906935e780503d1c3f73f5c908ce3a3ff29
Azul <azul@riseup.net>
2014-09-10 11:17:11 +0200

    zeus: add db_console, test_console and test_db tasks

---
+++ custom_plan.rb
@@ -0,0 +1,19 @@
+require 'zeus/rails'
+
+class CustomPlan < Zeus::Rails
+  def default_bundle_with_test_env
+    ::Rails.env = 'test'
+    ENV['RAILS_ENV'] = 'test'
+    default_bundle
+  end
+
+  def test_console
+    console
+  end
+
+  def test_db
+    dbconsole
+  end
+end
+
+Zeus.plan = CustomPlan.new
