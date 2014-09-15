9e1918ae66ce9474009aa826577c309856ddefe8
Azul <azul@riseup.net>
2014-09-15 12:52:10 +0200

    rails3.2: new environment settings

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -15,14 +15,14 @@ Crabgrass::Application.configure do
 
   config.active_support.deprecation = :log
 
-  ##
-  ## ADDED IN RAILS 3.1
-  ##
-
+  ## Rails 3.1
   # Configure static asset server for tests with Cache-Control for performance
   config.serve_static_assets = true
   config.static_cache_control = 'public, max-age=3600'
 
+  ## Rails 3.2
+  # Raise exception on mass assignment protection for Active Record models
+  config.active_record.mass_assignment_sanitizer = :strict
 
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper,
