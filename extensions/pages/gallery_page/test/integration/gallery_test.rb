45f276799719b0667b9e7848f5e73fb465de790f
Azul <azul@riseup.net>
2014-09-24 09:23:31 +0200

    use helpers for gallery page integration test
    still basically tests the same

--- extensions/pages/gallery_page/test/integration/gallery_test.rb
+++ extensions/pages/gallery_page/test/integration/gallery_test.rb
@@ -1,17 +1,9 @@
-require 'test_helper'
+require 'integration_test'
 
 class GalleryTest < IntegrationTest
   def test_create_gallery_with_images
     login
-
-    visit '/me/pages'
-    click_link I18n.t(:contribute_content_link)
-    click_link 'Gallery'
-
-    fill_in 'Title', :with => 'my pictures'
-    select 'rainbow', :from => 'Page Owner'
-    click_button 'Create Page'
-
-    assert_contain 'my pictures'
+    create_page :gallery,  :title => 'my pictures'
+    assert_content 'my pictures'
   end
 end
