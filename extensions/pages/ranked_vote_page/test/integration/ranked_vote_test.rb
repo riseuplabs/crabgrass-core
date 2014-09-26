2e86e7b92ebe3ac03394f2b1ad28c3985ca180b2
Azul <azul@riseup.net>
2014-09-26 09:14:22 +0200

    tune ranked vote integration test some

--- extensions/pages/ranked_vote_page/test/integration/ranked_vote_test.rb
+++ extensions/pages/ranked_vote_page/test/integration/ranked_vote_test.rb
@@ -2,6 +2,7 @@ require 'javascript_integration_test'
 
 class RateManyPageTest < JavascriptIntegrationTest
   include Integration::Possibility
+  include Integration::Navigation
 
   def setup
     super
@@ -25,16 +26,38 @@ class RateManyPageTest < JavascriptIntegrationTest
   end
 
   def test_voting
-    assert_page_header
     click_on 'Add new possibility'
     option, description = add_possibility
+    assert_not_voted_yet
+    vote
+    assert_voted
+    finish_voting
+    assert_first_choice_of(@user, option)
+  end
+
+  def vote
+    option_li = find('#sort_list_unvoted li.possible')
+    option_li.drag_to find('#sort_list_voted')
+  end
+
+  def finish_voting
+    click_on "Confirm Vote"
+  end
+
+  def assert_not_voted_yet
     within '#sort_list_voted' do
       assert_content 'None'
     end
-    option_li = find('#sort_list_unvoted li.possible')
-    option_li.drag_to find('#sort_list_voted')
+  end
+
+  def assert_voted
     within '#sort_list_voted' do
       assert_no_content 'None'
     end
+  end
+
+  def assert_first_choice_of(user, option)
+    click_on option
+    assert_content "first choice of : #{user.login}"
   end
 end
