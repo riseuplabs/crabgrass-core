780f27cc1ee4ea166f84fd40d56543d216517c48
Azul <azul@riseup.net>
2014-10-03 13:35:22 +0200

    test contexts_controller rather than dispatch_controller
    the latter is only remaining as a super class to inherit from for controllers that dispatch.

--- test/functional/contexts_controller_test.rb
+++ test/functional/contexts_controller_test.rb
@@ -1,10 +1,10 @@
 require_relative '../test_helper'
 
-class DispatchControllerTest < ActionController::TestCase
+class ContextsControllerTest < ActionController::TestCase
 
   def test_process_raises_not_found
     assert_raises ErrorNotFound do
-      get :dispatch, _context: 'pretty-sure-this-context-does-not-exist'
+      get :show, id: 'pretty-sure-this-context-does-not-exist'
     end
     assert_nil assigns[:user]
     assert_nil assigns[:group]
