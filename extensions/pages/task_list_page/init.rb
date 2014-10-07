4bcc71da94672683cce76ba861c4ce81fd40d8c6
Azul <azul@riseup.net>
2014-10-07 11:45:10 +0200

    clean up and fix task list functional tests

--- extensions/pages/task_list_page/init.rb
+++ extensions/pages/task_list_page/init.rb
@@ -9,8 +9,12 @@ define_page_type :TaskListPage, {
 
 Crabgrass.mod_routes do
   scope path: 'pages' do
-    resources :task_lists, controller: :task_list_page do
-      resources :tasks
+    resources :task_lists, controller: :task_list_page
+  end
+
+  scope path: 'pages/:page_id' do
+    resources :tasks do
+      post :sort, on: :collection
     end
   end
 end
