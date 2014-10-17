9dbe9c2b607bd993e37edadb342d917b8a2478a1
Azul <azul@riseup.net>
2014-10-17 10:05:41 +0200

    integration test for top navigation

---
+++ test/integration/top_navigation_test.rb
@@ -0,0 +1,21 @@
+require 'integration_test'
+
+class TopNavigationTest < IntegrationTest
+  include Integration::Navigation
+
+  def test_me_menu
+    login
+    entries = {
+      'Create Page' => 'Create a new page',
+      'Dashboard' => 'Recent Pages',
+      'Pages' => 'Active Filters',
+      'Messages' => 'Send Message',
+      'Settings' => 'Account Settings'
+    }
+    entries.each do |k, v|
+      find('#menu_me').click_on(k, visible: false)
+      assert_content v
+    end
+  end
+
+end
