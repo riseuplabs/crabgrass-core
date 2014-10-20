f46e6bda7d513694dce313cf0ce5789a8fdbfdcb
Azul <azul@riseup.net>
2014-10-20 14:43:12 +0200

    integration test for searching own pages
    also make fixtures more consistent by specifying a owner_name everywhere where there's
    an owner_id
    
    plus changed order of before_save callbacks for pages so that ensure_owner comes
    before the page_term creation. That way the page terms will also have an owner set.

--- test/integration/page_search_test.rb
+++ test/integration/page_search_test.rb
@@ -5,10 +5,25 @@ class PageSearchTest < JavascriptIntegrationTest
 
   def test_initial_search
     # new page so it shows up on top
-    own_page
     login users(:blue)
+    own_page
     click_on 'Pages'
     assert_content own_page.title
   end
 
+  def test_owned_by_me
+    login users(:blue)
+    own_page
+    click_on 'Pages'
+    click_on 'Own'
+    assert_content 'Owned By Me'
+    assert_text_of_all 'td.owner', user.login
+    assert_content own_page.title
+  end
+
+  def assert_text_of_all(selector, text)
+    all(selector).each do |elem|
+      assert_equal text, elem.text
+    end
+  end
 end
