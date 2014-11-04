e3c65471cf7369bbebd45718f594425e5ac3f4ac
Azul <azul@riseup.net>
2014-11-04 13:00:51 +0100

    make sure request queue is fifo
    Also wait for pending autocomplete requests before we end the test

--- test/integration/page_sidebar_test.rb
+++ test/integration/page_sidebar_test.rb
@@ -15,11 +15,13 @@ class PageSidebarTest < JavascriptIntegrationTest
   def test_sharing_with_user
     share_page_with users(:red)
     assert_page_users user, users(:red)
+    wait_for_ajax # autocomplete queries might still be pending
   end
 
   def test_sharing_with_group
     share_page_with groups(:animals)
     assert_page_groups groups(:animals)
+    wait_for_ajax # autocomplete queries might still be pending
   end
 
   def test_tagging
