b64a4b65f6297e792b88e80b320219d495dc22e1
Azul <azul@riseup.net>
2014-11-06 11:19:14 +0100

    @upart is needed for rendering sidebar - fixes 7834

--- test/integration/page_sidebar_test.rb
+++ test/integration/page_sidebar_test.rb
@@ -41,4 +41,34 @@ class PageSidebarTest < JavascriptIntegrationTest
     assert_content own_page.title
   end
 
+  def test_stars
+    star_page
+    assert_page_starred
+    remove_star_from_page
+    assert_page_not_starred
+  end
+
+  # regression test for #7834
+  def test_sharing_preserves_stars
+    star_page
+    assert_page_starred
+    share_page_with users(:red)
+    assert_page_starred
+  end
+
+  def star_page
+    click_on 'Add Star (0)'
+  end
+
+  def assert_page_starred
+    assert_selector '#star_li.star_16'
+  end
+
+  def remove_star_from_page
+    click_on 'Remove Star (1)'
+  end
+
+  def assert_page_not_starred
+    assert_selector '#star_li.star_empty_dark_16'
+  end
 end
