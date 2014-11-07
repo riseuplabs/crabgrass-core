14531f6f7a53621907306ad7a83cd5fa9cf5d595
Azul <azul@riseup.net>
2014-11-07 21:59:49 +0100

    Revert "page sidebar: refactor the link lists with partials and collections"
    This reverts commit 95f5c74b73446b16568376ecba7d6e6fdccccf60.
    
    Conflicts:
    	app/helpers/pages/sidebar_helper.rb

--- app/helpers/pages/sidebar_helper.rb
+++ app/helpers/pages/sidebar_helper.rb
@@ -5,9 +5,6 @@
 
 module Pages::SidebarHelper
 
-  Struct.new 'SidebarCommandGroup', :commands, :rule
-  Struct.new 'SidebarCommand', :content, :dom_id, :dom_class
-
   protected
 
   ##
@@ -16,67 +13,30 @@ module Pages::SidebarHelper
 
   def link_to_user_participation(upart)
     icon = case upart.access_sym
-           when :admin then 'tiny_wrench_16'
-           when :edit then 'tiny_pencil_16'
-           when :view then ''
-           end
+      when :admin then 'tiny_wrench_16'
+      when :edit then 'tiny_pencil_16'
+      when :view then ''
+    end
     label = content_tag :span, upart.user.display_name, class: icon
     link_to_entity(upart.user, avatar: 'xsmall', label: label)
   end
 
   def link_to_group_participation(gpart)
     icon = case gpart.access_sym
-           when :admin then 'tiny_wrench_16'
-           when :edit then 'tiny_pencil_16'
-           when :view then ''
-           end
+      when :admin then 'tiny_wrench_16'
+      when :edit then 'tiny_pencil_16'
+      when :view then ''
+    end
     label = content_tag :span, gpart.group.display_name, class: icon
     link_to_entity(gpart.group, avatar: 'xsmall', label: label)
   end
 
   ##
-  ## Sidebar Commands
-  ##
-
-  def sidebar_command_groups
-    return [] unless logged_in?
-    if @page.deleted?
-      [deleted_page_commands]
-    else
-      [participate_commands, access_commands, admin_commands].compact
-    end
-  end
-
-  def deleted_page_commands
-    group_commands undelete_page, destroy_page, details_page
-    # removed in core: history_line
-  end
-
-  def participate_commands
-    group_commands watch_page, star_page
-  end
-
-  def access_commands
-    group_commands share_page, notify_page, publish_page, rule: true
-  end
-
-  def admin_commands
-    group_commands delete_page, details_page, rule: true
-    # removed in core: move_line, history_line, view_line
-  end
-
-  def group_commands(*args)
-    options = args.extract_options!
-    Struct::SidebarCommandGroup.new args.compact, options[:rule]
-  end
-
-
-  ##
   ## SIDEBAR HELPERS
   ##
 
-  def sidebar_checkbox(text, url, options = {})
-    icon = options[:checked] ? 'check_on' : 'check_off'
+  def sidebar_checkbox(text, checked, url, options = {})
+    icon = checked ? 'check_on' : 'check_off'
     link_to_remote(
       text,
       {url: url, method: options[:method], complete: ''},
@@ -92,109 +52,93 @@ module Pages::SidebarHelper
   # checkbox to add/remove watched status
   #
 
-  def watch_page
-    return unless may_show_page?
-    Struct::SidebarCommand.new watch_checkbox, 'watch_li'
-  end
-
-  def watch_checkbox
-    existing_watch = (@upart and @upart.watch?) or false
-    checkbox_id = 'watch_li'
-    url = page_participations_path(@page, watch: (!existing_watch).inspect)
-    sidebar_checkbox I18n.t(:watch_checkbox), url,
-      id: checkbox_id, method: 'post', checked: existing_watch
+  def watch_line
+    if may_show_page?
+      existing_watch = (@upart and @upart.watch?) or false
+      li_id = 'watch_li'
+      checkbox_id = 'watch_checkbox'
+      url = page_participations_path(@page, :watch => (!existing_watch).inspect)
+      content_tag :li, :id => li_id do
+        sidebar_checkbox(I18n.t(:watch_checkbox), existing_watch, url, :id => checkbox_id, :method => 'post')
+      end
+    end
   end
 
   #
   # checkbox to add/remove public
   #
-
-  def publish_page
+  def public_line
     if may_admin_page?
-      Struct::SidebarCommand.new publish_checkbox, 'public_li'
+      url = page_attributes_path(@page, :public => (!@page.public?).inspect)
+      content_tag :li, :id => 'public_li' do
+        sidebar_checkbox(I18n.t(:public_checkbox),
+          @page.public?, url, :id => 'public_checkbox',
+          :method => 'put', :title => I18n.t(:public_checkbox_help))
+      end
     else
-      Struct::SidebarCommand.new is_public_checkbox
-    end
-  end
-
-  def publish_checkbox
-    url = page_attributes_path(@page, public: (!@page.public?).inspect)
-    sidebar_checkbox I18n.t(:public_checkbox), url,
-      id: 'public_li', checked: @page.public?,
-      method: 'put', title: I18n.t(:public_checkbox_help)
-  end
-
-  def is_public_checkbox
-    icon = @page.public? ? 'check_on_16' : 'check_off_16'
-    content_tag :span, class: "a icon #{icon}" do
-      :public_checkbox.t
+      checked = @page.public? ? 'check_on_16' : 'check_off_16'
+      content_tag :li do
+        content_tag :span, :class => "a icon #{checked}" do
+          :public_checkbox.t
+        end
+      end
     end
   end
 
   #
   # checkbox to add/remove star
   #
-
-  def star_page
-    return unless may_show_page?
-    Struct::SidebarCommand.new star_link, 'star_li'
-  end
-
-  def star_link
-    if @upart and @upart.star?
-      icon = 'star'
-      add = false
-      label = I18n.t(:remove_star_link, star_count: @page.stars_count)
-    else
-      icon = 'star_empty_dark'
-      add = true
-      label = I18n.t(:add_star_link, star_count: @page.stars_count)
+  def star_line
+    if may_show_page?
+      if @upart and @upart.star?
+        icon = 'star'
+        add = false
+        label = I18n.t(:remove_star_link, :star_count => @page.stars_count)
+      else
+        icon = 'star_empty_dark'
+        add = true
+        label = I18n.t(:add_star_link, :star_count => @page.stars_count)
+      end
+      url = page_participations_path(@page, :star => add.inspect)
+      content_tag :li, :id => 'star_li' do
+        link_to_remote(label, {:url => url, :method => 'post'}, {:icon => icon})
+      end
     end
-    url = page_participations_path(@page, star: add.inspect)
-    link_to_remote(label, {url: url, method: 'post'}, {icon: icon, id: 'star_li'})
   end
 
   #
   # used in the sidebar of deleted pages
   #
-  def undelete_page
-    return unless may_admin_page?
-    Struct::SidebarCommand.new undelete_link
-  end
-
-  def undelete_link
-    url = page_trash_path(@page, type: 'undelete')
-    link_to_remote_with_icon I18n.t(:undelete_from_trash),
-      url: url, method: 'put', icon: 'refresh'
+  def undelete_line
+    if may_admin_page?
+      content_tag :li do
+        link_to_remote_with_icon(I18n.t(:undelete_from_trash), :url => page_trash_path(@page, :type => 'undelete'), :method => 'put', :icon => 'refresh')
+      end
+    end
   end
 
   #
   # used in the sidebar of deleted pages
   #
-  def destroy_page
-    return unless may_admin_page?
-    Struct::SidebarCommand.new destroy_link
+  def destroy_line
+    if may_admin_page?
+      content_tag :li do
+        link_to_remote_with_icon(:destroy_page_via_shred.t, :icon => 'minus', :confirm => :destroy_confirmation.t(:thing => :page.t), :url => page_trash_path(@page, :type => 'destroy'), :method => 'put')
+      end
+    end
   end
 
-  def destroy_link
-    link_to_remote_with_icon :destroy_page_via_shred.t,
-      icon: 'minus',
-      confirm: :destroy_confirmation.t(thing: :page.t),
-      url: page_trash_path(@page, type: 'destroy'),
-      method: 'put'
-  end
+#  def view_line
+#    if @show_print != false
+#      printable = link_to I18n.t(:print_view_link), page_url(@page, :action => "print")
+#      content_tag :li, printable, :class => 'small_icon printer_16'
+#    end
+#  end
 
-  #  def view_line
-  #    if @show_print != false
-  #      printable = link_to I18n.t(:print_view_link), page_url(@page, :action => "print")
-  #      content_tag :li, printable, :class => 'small_icon printer_16'
-  #    end
-  #  end
-
-  #  def history_line
-  #    link = link_to I18n.t(:history), page_url(@page, :action => "page_history")
-  #    content_tag :li, link, :class => 'small_icon table_16'
-  #  end
+#  def history_line
+#    link = link_to I18n.t(:history), page_url(@page, :action => "page_history")
+#    content_tag :li, link, :class => 'small_icon table_16'
+#  end
 
   ##
   ## SIDEBAR COLLECTIONS
@@ -228,21 +172,15 @@ module Pages::SidebarHelper
   # create the <li></li> for a sidebar line that will open a popup when clicked
   # required options -- :id, :url, :label, :icon
   #
-
-  def popup_command(options)
-    Struct::SidebarCommand.new popup_link(options), options.delete(:id)
-  end
-
   def popup_line(options)
-    content_tag :li, id: options.delete(:id) do
-      popup_link(options)
-    end
-  end
-
-  def popup_link(options)
     options[:after_hide] =
       "if (typeof(afterHide) != 'undefined' || afterHide != null) { afterHide(); }"
-    link_to_modal(options.delete(:label), options)
+    content_tag :li, id: options.delete(:id) do
+      link_to_modal(
+        options.delete(:label),
+        options
+      )
+    end
   end
 
   def edit_attachments_line
@@ -267,9 +205,9 @@ module Pages::SidebarHelper
     end
   end
 
-  def share_page
+  def share_line
     if may_admin_page?
-      popup_command(
+      popup_line(
         id: 'share_li',
         icon: 'group',
         label: I18n.t(:share_page_link, page_class: :page.t),
@@ -278,9 +216,9 @@ module Pages::SidebarHelper
     end
   end
 
-  def notify_page
+  def notify_line
     if may_edit_page?
-      popup_command(
+      popup_line(
         id: 'notify_li',
         icon: 'whistle',
         label: I18n.t(:notify_page_link),
@@ -289,9 +227,9 @@ module Pages::SidebarHelper
     end
   end
 
-  def delete_page
+  def delete_line
     if may_admin_page?
-      popup_command(
+      popup_line(
         id: 'trash_li',
         icon: 'trash',
         label: I18n.t(:delete_page_link, page_class: :page.t),
@@ -300,9 +238,9 @@ module Pages::SidebarHelper
     end
   end
 
-  def details_page
+  def details_line
     if may_edit_page?
-      popup_command(
+      popup_line(
         id: 'details_li',
         icon: 'page_admin',
         label: I18n.t(:page_details_link, page_class: :page.t),
