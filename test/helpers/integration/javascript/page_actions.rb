7e89b096b57d08898a6d12a2aa798130dcaea7c2
Azul <azul@riseup.net>
2014-11-11 10:16:41 +0100

    prevent dangling autocomplete requests
    dangling ajax requests mess up other tests.
    
     We fill in the autocomplete character by character.
     As soon as the thing we are looking for shows up we click it.
     Filling in the whole field would trigger tons of autocomplete requests.

--- test/helpers/integration/javascript/page_actions.rb
+++ test/helpers/integration/javascript/page_actions.rb
@@ -84,13 +84,23 @@ module PageActions
     wait_for_ajax
   end
 
+  #
+  # We fill in the autocomplete character by character.
+  # As soon as the thing we are looking for shows up we click it.
+  # Filling in the whole field would trigger tons of autocomplete requests.
+  #
   def autocomplete(field, options)
+    chars ||= 1
     # the space is a work around as the first letter may get cut off
-    fill_in field, with: ' ' + options[:with][0,2]
+    fill_in field, with: ' ' + options[:with][0,chars]
     # poltergeist will not keep the element focussed.
     # But when we loose focus the autocomplete won't show.
     execute_script("$('recipient_name').focus();")
     find('.autocomplete em', text: options[:with]).click
+  rescue Capybara::ElementNotFound
+    chars +=1
+    raise if chars > 3
+    retry
   end
 
 end
