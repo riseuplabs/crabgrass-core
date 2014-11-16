51631668b996f464bea11164f7c12090ef50f9ab
Azul <azul@riseup.net>
2014-11-16 17:59:59 +0100

    failing: more integration tests for people directory

--- test/integration/people_directory_test.rb
+++ test/integration/people_directory_test.rb
@@ -8,12 +8,27 @@ class PeopleDirectoryTest < JavascriptIntegrationTest
     login
   end
 
-  def test_search
+  def test_contacts
     click_on 'People'
-    click_on 'Search'
+    assert_no_autocomplete 'user_name', with: 'red'
+    autocomplete 'user_name', with: 'orange'
+    assert_content 'Orange'
+  end
+
+  def test_peers
+    click_on 'People'
+    click_on 'Peers'
+    assert_no_autocomplete 'user_name', with: 'aaron'
     autocomplete 'user_name', with: 'red'
     assert_content 'Red'
   end
 
+  def test_search
+    click_on 'People'
+    click_on 'Search'
+    assert_no_autocomplete 'user_name', with: 'black'
+    autocomplete 'user_name', with: 'aaron'
+    assert_content 'Aaron'
+  end
 
 end
