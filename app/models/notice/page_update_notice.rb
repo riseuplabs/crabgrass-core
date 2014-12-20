95e1325b191eef629579438eb437955039c64f8a
Azul <azul@riseup.net>
2014-12-20 13:02:17 +0100

    notify watching users on page updates

---
+++ app/models/notice/page_update_notice.rb
@@ -0,0 +1,19 @@
+class PageUpdateNotice < PageNotice
+
+  def display_title
+    I18n.t("page_updated", data).html_safe
+  end
+
+  def display_body_as_quote?
+    false
+  end
+
+  def display_body
+    ""
+  end
+
+  def display_label
+    I18n.t "page_update"
+  end
+
+end
