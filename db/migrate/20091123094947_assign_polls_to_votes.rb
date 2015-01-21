015d00053d0be73158ee67746299614252d21e55
Azul <azul@riseup.net>
2015-01-21 16:50:56 +0100

    table names use plurals
    no idea how this one sneaked in.

--- db/migrate/20091123094947_assign_polls_to_votes.rb
+++ db/migrate/20091123094947_assign_polls_to_votes.rb
@@ -3,8 +3,8 @@ class AssignPollsToVotes < ActiveRecord::Migration
   def self.up
     Possible.connection.execute <<-EOSQL
       UPDATE votes, possibles
-        SET votes.votable_type = 'Poll', votes.votable_id = possible.poll_id
-        WHERE votes.possible_id = possible.id
+        SET votes.votable_type = 'Poll', votes.votable_id = possibles.poll_id
+        WHERE votes.possible_id = possibles.id
     EOSQL
   end
 
