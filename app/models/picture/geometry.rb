74acc8a90f2bddbee2b32d122342c5d542660497
Azul <azul@riseup.net>
2015-01-29 10:30:16 +0100

    validate: add record validation task, fix picture namespace

--- app/models/picture/geometry.rb
+++ app/models/picture/geometry.rb
@@ -17,7 +17,6 @@
 # (2) Array, like [50,0,0,0]
 # (3) String, like "50-0-0-0"
 #
-class Picture
   #
   # Picture::Geometry class
   #
@@ -25,129 +24,128 @@ class Picture
   # This is important, because we might have got the geometry source
   # from a url.
   #
-  class Geometry
+class Picture::Geometry
 
-    attr_accessor :min_width, :max_width, :min_height, :max_height
+  attr_accessor :min_width, :max_width, :min_height, :max_height
 
-    def initialize(source=nil)
-      set_limits *limits_from_source(source)
+  def initialize(source=nil)
+    set_limits *limits_from_source(source)
+  end
+
+  def self.[](geo)
+    if geo.class == self
+      return geo
+    else
+      return new(geo)
     end
+  end
 
-    def self.[](geo)
-      if geo.class == self
-        return geo
-      else
-        return new(geo)
-      end
+  def limits_from_source(source=nil)
+    case source
+    when Hash
+      [source[:min_width], source[:max_width], source[:min_height], source[:max_height]]
+    when  Array
+      source
+    when String
+      source.split('-')
+    else
+      []
     end
+  end
 
-    def limits_from_source(source=nil)
-      case source
-      when Hash
-        [source[:min_width], source[:max_width], source[:min_height], source[:max_height]]
-      when  Array
-        source
-      when String
-        source.split('-')
-      else
-        []
-      end
+  def set_limits(minw=nil, maxw=nil, minh=nil, maxh=nil)
+    self.min_width  = minw.to_i if minw
+    self.min_width  = nil       if self.min_width == 0
+    self.max_width  = maxw.to_i if maxw
+    self.max_width  = nil       if self.max_width == 0
+    self.min_height = minh.to_i if minh
+    self.min_height = nil       if self.min_height == 0
+    self.max_height = maxh.to_i if maxh
+    self.max_height = nil       if self.max_height == 0
+  end
+
+  def empty?
+    not any?
+  end
+
+  def any?
+    min_width || max_width || min_height || max_height
+  end
+
+  def to_s
+    empty? ? 'full' : to_a.join('-')
+  end
+
+  def to_a
+    empty? ? [] : [min_width||0, max_width||0, min_height||0, max_height||0]
+  end
+
+  #
+  # Convert geometry definition into graphic magick compatible
+  # resize options. The logic is complicated, but it tries
+  # to do the right thing.
+  #
+  # summary from gm man page on -geometry:
+  #
+  #   1. WxH  -- max dimensions, resize with aspect ratio intact
+  #   2. WxH^ -- min dimensions, resize with aspect ratio intact
+  #   3. WxH! -- force dimensions exactly
+  #   4. Wx   -- resize, maintain ratio, auto set height
+  #   5. xH   -- resize, maintain ratio, auto set width
+  #   6. WxH> -- shrink if bigger (W or H)
+  #   7. WxH< -- expand if smaller (W and H)
+  #
+  def gm_size_param_from(orig_size)
+    width = orig_size[0]
+    height = orig_size[1]
+
+    new_width = scale_to_fit(min_width, width, max_width)
+    new_height = scale_to_fit(min_height, height, max_height)
+
+    if new_width.to_f / width > new_height.to_f / height
+      scale_by_width(width, new_width)
+    elsif new_height > 0
+      scale_by_height(height, new_height)
     end
+  end
 
-    def set_limits(minw=nil, maxw=nil, minh=nil, maxh=nil)
-      self.min_width  = minw.to_i if minw
-      self.min_width  = nil       if self.min_width == 0
-      self.max_width  = maxw.to_i if maxw
-      self.max_width  = nil       if self.max_width == 0
-      self.min_height = minh.to_i if minh
-      self.min_height = nil       if self.min_height == 0
-      self.max_height = maxh.to_i if maxh
-      self.max_height = nil       if self.max_height == 0
+  def gm_crop_param
+    if max_width or max_height
+      "%sx%s" % [max_width||10000000, max_height||10000000]
+    else
+      nil
     end
+  end
 
-    def empty?
-      not any?
+  protected
+
+  def scale_to_fit(min, current, max)
+    if min && current < min
+      min    # scale bigger
+    elsif max && current > max
+      max    # scale smaller
+    else
+      0
     end
+  end
 
-    def any?
-      min_width || max_width || min_height || max_height
+  def scale_by_width(old_width, new_width)
+    if old_width < new_width
+      "%sx^" % new_width # bigger
+    elsif old_width == new_width
+      nil
+    else
+      "%sx" % new_width  # smaller
     end
+  end
 
-    def to_s
-      empty? ? 'full' : to_a.join('-')
-    end
-
-    def to_a
-      empty? ? [] : [min_width||0, max_width||0, min_height||0, max_height||0]
-    end
-
-    #
-    # Convert geometry definition into graphic magick compatible
-    # resize options. The logic is complicated, but it tries
-    # to do the right thing.
-    #
-    # summary from gm man page on -geometry:
-    #
-    #   1. WxH  -- max dimensions, resize with aspect ratio intact
-    #   2. WxH^ -- min dimensions, resize with aspect ratio intact
-    #   3. WxH! -- force dimensions exactly
-    #   4. Wx   -- resize, maintain ratio, auto set height
-    #   5. xH   -- resize, maintain ratio, auto set width
-    #   6. WxH> -- shrink if bigger (W or H)
-    #   7. WxH< -- expand if smaller (W and H)
-    #
-    def gm_size_param_from(orig_size)
-      width = orig_size[0]
-      height = orig_size[1]
-
-      new_width = scale_to_fit(min_width, width, max_width)
-      new_height = scale_to_fit(min_height, height, max_height)
-
-      if new_width.to_f / width > new_height.to_f / height
-        scale_by_width(width, new_width)
-      elsif new_height > 0
-        scale_by_height(height, new_height)
-      end
-    end
-
-    def gm_crop_param
-      if max_width or max_height
-        "%sx%s" % [max_width||10000000, max_height||10000000]
-      else
-        nil
-      end
-    end
-
-    protected
-
-    def scale_to_fit(min, current, max)
-      if min && current < min
-        min    # scale bigger
-      elsif max && current > max
-        max    # scale smaller
-      else
-        0
-      end
-    end
-
-    def scale_by_width(old_width, new_width)
-      if old_width < new_width
-        "%sx^" % new_width # bigger
-      elsif old_width == new_width
-        nil
-      else
-        "%sx" % new_width  # smaller
-      end
-    end
-
-    def scale_by_height(old_height, new_height)
-      if old_height < new_height
-        "x%s^" % new_height # bigger
-      elsif old_height == new_height
-        nil
-      else
-        "x%s" % new_height  # smaller
-      end
+  def scale_by_height(old_height, new_height)
+    if old_height < new_height
+      "x%s^" % new_height # bigger
+    elsif old_height == new_height
+      nil
+    else
+      "x%s" % new_height  # smaller
     end
   end
 end
--- app/models/picture/storage.rb
+++ app/models/picture/storage.rb
@@ -1,69 +1,68 @@
-class Picture
-  class Storage
+class Picture::Storage
 
-    def initialize(picture)
-      @picture = picture
-    end
+  def initialize(picture)
+    @picture = picture
+  end
 
-    def private_path(geometry=nil)
-      File.join(private_directory, file_name(geometry))
-    end
+  def private_path(geometry=nil)
+    File.join(private_directory, file_name(geometry))
+  end
 
-    def public_path(geometry=nil)
-      File.join(public_directory, file_name(geometry))
-    end
+  def public_path(geometry=nil)
+    File.join(public_directory, file_name(geometry))
+  end
 
-    def url(geometry)
-      File.join(URL_ROOT, directory, file_name(geometry))
-    end
+  def url(geometry)
+    File.join(URL_ROOT, directory, file_name(geometry))
+  end
 
-    def dimensions(geometry)
-      path = private_path(geometry)
-      width, height = GraphicsMagickTransmogrifier.new.dimensions(path)
-      [(width||0).to_i, (height||0).to_i]
-    end
+  def dimensions(geometry)
+    path = private_path(geometry)
+    width, height = GraphicsMagickTransmogrifier.new.dimensions(path)
+    [(width||0).to_i, (height||0).to_i]
+  end
 
-    def average_color
-      GraphicsMagickTransmogrifier.new.average_color(private_path)
-    end
+  def average_color
+    GraphicsMagickTransmogrifier.new.average_color(private_path)
+  end
 
-    def destroy_files
-      FileUtils.rm_rf(private_directory) if File.exist?(private_directory)
-      FileUtils.rm(public_directory) if File.exist?(public_directory)
-    end
+  def destroy_files
+    FileUtils.rm_rf(private_directory) if File.exist?(private_directory)
+    FileUtils.rm(public_directory) if File.exist?(public_directory)
+  end
 
-    def destroy_file(geometry)
-      FileUtils.rm(public_path(geometry))
-      FileUtils.rm(private_path(geometry))
-    end
+  def destroy_file(geometry)
+    FileUtils.rm(public_path(geometry))
+    FileUtils.rm(private_path(geometry))
+  end
 
-    #
-    # Ensures storage directory exists for this Picture
-    #
-    def allocate_directory
-      FileUtils.mkdir_p(private_directory) unless File.exist?(private_directory)
-      add_symlink # for now, all Pictures are public.
-    end
+  #
+  # Ensures storage directory exists for this Picture
+  #
+  def allocate_directory
+    FileUtils.mkdir_p(private_directory) unless File.exist?(private_directory)
+    add_symlink # for now, all Pictures are public.
+  end
 
-    #
-    # creates a symlink from the private storage to a public storage
-    #
-    # this makes the picture public
-    #
-    def add_symlink
-      unless File.exist?(public_directory)
+  #
+  # creates a symlink from the private storage to a public storage
+  #
+  # this makes the picture public
+  #
+  def add_symlink
+    unless File.exist?(public_directory)
 
-        public_directory_parent = File.dirname(public_directory)
-        unless File.exist?(public_directory_parent)
-          FileUtils.mkdir_p(public_directory_parent)
-        end
-
-        real_private_path = Pathname.new(private_directory).realpath
-        real_public_path  = Pathname.new(public_directory_parent).realpath
-        public_to_private = real_private_path.relative_path_from(real_public_path)
-        FileUtils.ln_s(public_to_private, real_public_path)
+      public_directory_parent = File.dirname(public_directory)
+      unless File.exist?(public_directory_parent)
+        FileUtils.mkdir_p(public_directory_parent)
       end
+
+      real_private_path = Pathname.new(private_directory).realpath
+      real_public_path  = Pathname.new(public_directory_parent).realpath
+      public_to_private = real_private_path.relative_path_from(real_public_path)
+      FileUtils.ln_s(public_to_private, real_public_path)
     end
+  end
 
   #
   # removes symlink that links private and public directories
@@ -78,56 +77,49 @@ class Picture
 
   protected
 
-    #
-    # the relative path of the directory where all the files live for
-    # this picture. (returned as an array for use in File.join)
-    #
-    # e.g. id of 12345 produces ['0001','2345']
-    #
-    def directory
-      ("%08d" % @picture.id).scan(/..../)
-    end
-
-    #
-    # the private filesystem path of this picture
-    #
-    def private_directory
-      File.join(PICTURE_PRIVATE_STORAGE, directory)
-    end
-
-    #
-    # the public filesystem path of this picture
-    #
-    def public_directory
-      File.join(PICTURE_PUBLIC_STORAGE, directory)
-    end
-
-    #
-    # returns a file name appropriate for the specified
-    # geometry.
-    #
-    # eg:
-    #   {:max_height => 100}  --> '0-0-0-100.jpg'
-    #   nil                   --> 'full.jpg'
-    #
-    #
-    def file_name(geometry)
-      if geometry
-        geometry = geometry.is_a?(Geometry) ? geometry : Geometry.new(geometry)
-        geometry.to_s + '.' + ext
-      else
-        'full.' + ext
-      end
-    end
-
-
-    #
-    # returns the file extension suitable for this content_type
-    #
-    def ext
-      Media::MimeType.extension_from_mime_type(@picture.content_type).to_s
-    end
-
+  #
+  # the relative path of the directory where all the files live for
+  # this picture. (returned as an array for use in File.join)
+  #
+  # e.g. id of 12345 produces ['0001','2345']
+  #
+  def directory
+    ("%08d" % @picture.id).scan(/..../)
   end
-end
 
+  #
+  # the private filesystem path of this picture
+  #
+  def private_directory
+    File.join(PICTURE_PRIVATE_STORAGE, directory)
+  end
+
+  #
+  # the public filesystem path of this picture
+  #
+  def public_directory
+    File.join(PICTURE_PUBLIC_STORAGE, directory)
+  end
+
+  #
+  # returns a file name appropriate for the specified
+  # geometry.
+  #
+  # eg:
+  #   {:max_height => 100}  --> '0-0-0-100.jpg'
+  #   nil                   --> 'full.jpg'
+  #
+  #
+  def file_name(geometry)
+    Picture::Geometry[geometry].to_s + ext
+  end
+
+
+  #
+  # returns the file extension suitable for this content_type
+  #
+  def ext
+    Media::MimeType.extension_from_mime_type(@picture.content_type).to_s
+  end
+
+end
