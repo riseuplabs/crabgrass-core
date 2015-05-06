a2aabaea4127a451d618e09ae77f75696c26ecb2
Azul <azul@riseup.net>
2015-05-06 12:49:26 +0200

    turn all pages into engines

--- extensions/pages/event_page/config/routes.rb
+++ extensions/pages/event_page/config/routes.rb
@@ -1,14 +1,4 @@
-define_page_type :EventPage, {
-  controller: 'event_page',
-  creation_controller: 'create_event_page',
-  model: 'Event',
-  form_sections: ['event'],
-  icon: 'page_event',
-  class_group: ['event'],
-  order: 10
-}
-
-Crabgrass.mod_routes do
+Rails.application.routes.draw do
   scope path: 'pages' do
     resources :events,
       only: [:show, :edit, :update],
--- extensions/pages/task_list_page/config/routes.rb
+++ extensions/pages/task_list_page/config/routes.rb
@@ -1,13 +1,4 @@
-
-define_page_type :TaskListPage, {
-  controller: ['task_list_page', 'tasks'],
-  model: 'TaskList',
-  icon: 'page_tasks',
-  class_group: ['planning', 'task'],
-  order: 3
-}
-
-Crabgrass.mod_routes do
+Rails.application.routes.draw do
   scope path: 'pages' do
     resources :task_lists, controller: :task_list_page, only: :show
   end
