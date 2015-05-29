85d521204a3b4171601d7eb85cb398ccf47cb298
Azul <azul@riseup.net>
2015-05-29 13:09:52 +0200

    cleanup: Groups::GroupsController

--- app/controllers/groups/groups_controller.rb
+++ app/controllers/groups/groups_controller.rb
@@ -6,9 +6,7 @@ class Groups::GroupsController < Groups::BaseController
   def fetch_group() super if action? :destroy end
   protected :fetch_group
 
-  before_filter :force_type,  only: ['new', 'create']
   before_filter :initialize_group,  only: ['new', 'create']
-  before_filter :fetch_member_group, only: 'create'
 
   guard :may_ALIAS_group?
 
@@ -74,8 +72,8 @@ class Groups::GroupsController < Groups::BaseController
   end
 
   def group_type
-    if %w/group network council committee/.include? params[:type].to_s
-      params[:type].to_sym
+    if Conf.networks && params[:type].to_s == 'network'
+      :network
     else
       :group
     end
@@ -83,38 +81,23 @@ class Groups::GroupsController < Groups::BaseController
   helper_method :group_type
 
   def group_class
-    case group_type
-      when :group then Group
-      when :network then Network
-      when :committee then Committee
-      when :council then Council
-    end
-  end
-
-  #
-  # if some non-normal groups are disabled, then
-  # we force type 'group'
-  #
-  def force_type
-    if !Conf.committees and !Conf.networks and !Conf.councils
-      params[:type] = 'group'
-    end
+    group_type.to_s.capitalize.constantize
   end
 
   def initialize_group
     @group = group_class.new group_params
     @group.created_by = current_user
+    @group.initial_member_group = member_group if group_type == :network
   end
 
-  def fetch_member_group
-    if @group.network? and @group.initial_member_group
-      raise_denied unless current_user.may?(:admin, @group.initial_member_group)
+  def member_group
+    name = params.fetch(:group, {})[:initial_member_group]
+    Group.where(name: name).first.tap do |group|
+      current_user.may!(:admin, group)
     end
   end
 
   def group_params
-    permitted = [:name, :full_name, :language]
-    permitted << :initial_member_group if group_type == :network
-    params.fetch(:group, {}).permit *permitted
+    params.fetch(:group, {}).permit :name, :full_name, :language
   end
 end
