a7e8f63ec91972f33e4841adbbca8418f8b31f2c
Azul <azul@riseup.net>
2015-06-03 11:08:51 +0200

    prevent 500 for links that still use url encodes iso chars
    We used some iso encoding before and that would turn some charectars into
    %F3 and similar when url encoded.
    
    Now that we switched to utf-8 some of those links still exist but lead to
    utf-8 errors as characters above \xF0 are invalid. Since the exact filename
    is not relevant for the assets controller anyway and this has been where we
    saw the issue in production we will just drop the invalid chars in the utf-8
    encoding.

--- test/functional/assets_controller_test.rb
+++ test/functional/assets_controller_test.rb
@@ -9,6 +9,14 @@ class AssetsControllerTest < ActionController::TestCase
     assert_login_required
   end
 
+  def test_get_with_escaped_chars
+    ImageAsset.any_instance.stubs(:public?).returns(false)
+    @controller.stubs(:authorized?).returns(true)
+    asset = FactoryGirl.create :image_asset
+    get :show, id: asset.id, path: asset.basename + '\xF3'
+    assert_response :success
+  end
+
   def test_thumbnail_get
     ImageAsset.any_instance.stubs(:public?).returns(false)
     asset = FactoryGirl.create :image_asset
