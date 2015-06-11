4835cd150f573d4e5dee3defda0725e5e3cce1ae
Azul <azul@riseup.net>
2015-06-11 09:59:12 +0200

    Test group destruction and approve requests without js

---
+++ test/integration/group_destruction_test.rb
@@ -0,0 +1,24 @@
+require 'integration_test'
+
+class GroupDestructionTest < IntegrationTest
+
+  def test_visit_own_group
+    @user = users(:blue)
+    login
+    visit '/animals'
+    click_on 'Settings'
+    click_on 'Structure'
+    click_on 'Destroy animals'
+    assert_content 'Request to Destroy Group was sent to animals'
+    assert_no_content 'Approve'
+    logout
+    @user = users(:penguin)
+    login
+    click_on 'Show Request'
+    click_on 'Approve'
+    click_on 'Groups'
+    assert_no_content 'animals'
+  end
+
+
+end
