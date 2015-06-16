2a8f05cab8ecc17539d2e40007a28f707a4eb422
Azul <azul@riseup.net>
2015-06-16 11:58:25 +0200

    more robust gallery integration test
    make sure the upload of the first image finished before uploading the
    second one.

--- extensions/pages/gallery_page/test/integration/gallery_test.rb
+++ extensions/pages/gallery_page/test/integration/gallery_test.rb
@@ -9,8 +9,9 @@ class GalleryTest < JavascriptIntegrationTest
     create_page :gallery,  title: 'my pictures'
     assert_content 'my pictures'
     attach_file 'upload-input', fixture_file('photo.jpg')
-    attach_file 'upload-input', fixture_file('beé.jpg')
     assert_content 'photo'
+    attach_file 'upload-input', fixture_file('beé.jpg')
+    assert_content 'beé'
     click_page_tab 'Show'
     first('.control_image_asset .thumbnail').click
     assert_content 'Image 1 of 2'
