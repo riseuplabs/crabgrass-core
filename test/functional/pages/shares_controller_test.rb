2c80e61add2026616fa85a9a5f98ba906fdea12a
Azul <azul@riseup.net>
2015-06-18 09:58:30 +0200

    test page sharing with page history creation
    Starting with tests that test the integration of the Pages::SharesController
    and the PageHistory tracking. The method called to track the creation
    will change soon. So this will make sure things still behave the same.

--- test/functional/pages/shares_controller_test.rb
+++ test/functional/pages/shares_controller_test.rb
@@ -7,7 +7,7 @@ class Pages::SharesControllerTest < ActionController::TestCase
     @recipient = FactoryGirl.create(:user)
   end
 
-  def test_autocomplete_user_from_new_page
+  def test_autocomplete_user_for_new_page
     login_as @owner
     xhr :post, :update, recipient: {name: @recipient.name, access: :admin},
       page_id: "0",
@@ -16,5 +16,48 @@ class Pages::SharesControllerTest < ActionController::TestCase
       format: :js
     assert @response.body.include?(@recipient.login)
     assert_template partial: '_add_recipient'
+  end
+
+  def test_share_page_with_multiple_recipients
+    page = FactoryGirl.create :page, created_by: @owner
+    login_as @owner
+    admin = {access: 'admin'}
+    assert_difference "PageHistory.count", 2 do
+      xhr :post, :update, share_button: true,
+        recipients: {blue: admin, animals: admin},
+        page_id: page.id,
+        mode: 'share',
+        format: :js
+    end
+  end
+
+  def test_share_page_with_group
+    page = FactoryGirl.create :page, created_by: @owner
+    login_as @owner
+    admin = {access: 'admin'}
+    assert_difference "PageHistory.count" do
+      xhr :post, :update, share_button: true,
+        recipients: {animals: admin},
+        page_id: page.id,
+        mode: 'share',
+        format: :js
+    end
+    assert_equal "page_history_granted_group_full_access",
+      PageHistory.last.description_key
+  end
+
+  def test_share_page_with_user
+    page = FactoryGirl.create :page, created_by: @owner
+    login_as @owner
+    admin = {access: 'admin'}
+    assert_difference "PageHistory.count" do
+      xhr :post, :update, share_button: true,
+        recipients: {blue: admin},
+        page_id: page.id,
+        mode: 'share',
+        format: :js
+    end
+    assert_equal "page_history_granted_user_full_access",
+      PageHistory.last.description_key
   end
 end
