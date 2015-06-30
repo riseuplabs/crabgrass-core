7310d5601815583c0190bb121394889722d188a0
elijah <elijah@riseup.net>
2015-06-29 23:15:52 -0700

    consider_all_requests_local should be true in dev mode.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,7 +21,7 @@ Crabgrass::Application.configure do
   config.whiny_nils = true
   config.assets.compress = false
   config.assets.debug = true
-  config.consider_all_requests_local = false
+  config.consider_all_requests_local = true
   #config.action_view.debug_rjs                         = true
   config.action_controller.perform_caching              = false
   config.action_mailer.raise_delivery_errors = false
