db0484c413440b6bbf1d569eb19e933ac2aef9c6
Aliaksandr Buhayeu <aliaksandr.buhayeu@gmail.com>
2015-07-04 18:20:50 +0300

    Missed notification for contact request
    If the user A requests the user B to be a contact
    user B has no notification about it.
    So the only way to approve - check the "Requests" tab manually.
    
    Bug: 9675
    Link: https://labs.riseup.net/code/issues/9675

---
+++ test/functional/people/friend_requests_controller_test.rb
@@ -0,0 +1,26 @@
+require_relative '../../test_helper'
+
+class People::FriendRequestsControllerTest < ActionController::TestCase
+
+  fixtures :users
+
+  def test_new_contact_request_notifies_recipient
+    requesting = users(:blue)
+    recipient  = users(:yellow)
+    login_as requesting
+
+    # Stub permission access for current test case
+    def requesting.may?(*args)
+      true
+    end
+
+    assert_difference 'RequestNotice.count', 1 do
+      xhr :post, :create, person_id: recipient.login
+    end
+
+    notice = RequestNotice.last
+    assert_equal recipient.id, notice.user_id
+    assert_equal 'request_to_friend', notice.data[:title]
+  end
+
+end
