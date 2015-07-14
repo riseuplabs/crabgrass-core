10fc21db1c09b1fda4c53a9a9661cd3d970b5900
Azul <azul@riseup.net>
2015-07-14 10:25:47 +0200

    drop unused sweepers

--- app/controllers/groups/avatars_controller.rb
+++ app/controllers/groups/avatars_controller.rb
@@ -3,7 +3,6 @@ class Groups::AvatarsController < Groups::BaseController
   include_controllers 'common/avatars'
   include_controllers 'common/always_perform_caching'
   before_filter :setup
-  cache_sweeper :group_sweeper
 
   guard :allow
 
