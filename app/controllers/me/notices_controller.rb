5b6ee641d9748b12849df15bfda0c61d1f9612e0
Aliaksandr Buhayeu <aliaksandr.buhayeu@gmail.com>
2015-07-23 03:42:35 +0300

    Homepage queries optimization
    Fix for N + 1 problem during not preloaded data.
    
    Logs comparission https://gist.github.com/aliaksandrb/7fa318f10cce05446d50

--- app/controllers/me/notices_controller.rb
+++ app/controllers/me/notices_controller.rb
@@ -5,8 +5,12 @@ class Me::NoticesController < Me::BaseController
   def index
     @notices = Notice.for_user(current_user).
       dismissed(params[:view] == 'old').
+      includes(:noticable).
       paginate(pagination_params.merge(order: "created_at desc"))
-    @pages = current_user.pages.not_deleted.order('pages.updated_at desc').limit(30)
+
+    @pages = current_user.pages.not_deleted.
+      includes(:owner, :updated_by).
+      order('pages.updated_at desc').limit(30)
   end
 
   def show
