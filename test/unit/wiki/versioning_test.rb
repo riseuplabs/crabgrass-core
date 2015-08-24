5d8afc71c233ac514b6a284224a39579da353f24
elijah <elijah@riseup.net>
2015-08-24 02:01:51 -0700

    port wiki/locking_test and wiki/versioning_test to minitest

--- test/unit/wiki/versioning_test.rb
+++ test/unit/wiki/versioning_test.rb
@@ -1,162 +1,130 @@
-require File.dirname(__FILE__) + '/../test_helper'
+require_relative '../test_helper'
 
-module Wiki::VersioningTest
-  def self.included(base)
-    base.instance_eval do
-      context "A new Wiki" do
-        setup do
-          @wiki = Wiki.new
-        end
+class Wiki::VersioningTest < ActiveSupport::TestCase
+  fixtures :users, :wikis
+  include WikiTestHelper
 
-        context "before saving" do
-          should "have no versions" do
-            assert @wiki.versions.empty?
-          end
-        end
-
-        # test chat changing body updates versions only when
-        # a new user does it
-        context "saved with a body by a user" do
-          setup do
-            @wiki.update_attributes!(body: 'hi', user: @user)
-          end
-
-          should_change("versions count", from: 0, to: 1) { @wiki.versions.size }
-
-          context "and then saved with the same body by different user" do
-            setup do
-              @wiki.update_attributes!(user: @different_user)
-            end
-
-            should_not_change("versions count") { @wiki.versions.size }
-          end
-
-          context "and saved with a new body by a different user" do
-            setup do
-              @wiki.update_attributes!(body: 'hi there', user: @different_user)
-            end
-
-            should_change("versions count", from: 1, to: 2) { @wiki.versions.size }
-
-            should_have_latest_body 'hi there'
-            should_have_latest_body_html '<p>hi there</p>'
-            should_have_latest_raw_structure(WikiTest.raw_structure_for_n_byte_body(8))
-          end
-
-          context "and saved with a new body by the same user" do
-            setup do
-              @wiki.body = 'hey you'
-              @wiki.user = users(:blue)
-              assert_nothing_raised { @wiki.save! }
-            end
-
-            should_not_change("versions count") { @wiki.versions.size }
-
-            should_have_latest_body 'hey you'
-            should_have_latest_body_html '<p>hey you</p>'
-            should_have_latest_raw_structure(raw_structure_for_n_byte_body(7))
-          end
-        end
-
-        ### wiki should not create new versions on top of a blank version
-        ['', nil].each do |initial_body|
-          context "saved with #{initial_body.inspect} body by a user" do
-            setup do
-              @wiki.update_attributes!(body: initial_body, user: @user)
-            end
-
-            should_change("versions count", from: 0, to: 1) { @wiki.versions.size }
-
-            should_have_latest_body initial_body
-            should_have_latest_body_html ''
-            should_have_latest_raw_structure(WikiTest.raw_structure_for_n_byte_body(0))
-
-            context "and then saved with new body by a different user" do
-              setup do
-                @wiki.update_attributes!(body: 'oi', user: @user)
-              end
-
-              should_not_change("versions count") { @wiki.versions.size }
-              should_have_latest_body 'oi'
-            end
-
-            context "and then saved with new body by the same user" do
-              setup do
-                @wiki.update_attributes!(body: 'oi', user: @user)
-              end
-
-              should_not_change("versions count") { @wiki.versions.size }
-              should_have_latest_body 'oi'
-            end
-          end
-        end
-
-        context "saved with 'oi', '' (blank body) and 'vey' bodies by alternating users" do
-          setup do
-            @wiki.update_attributes!(body: 'oi', user: @user)
-            @wiki.update_attributes!(body: '', user: @different_user)
-            @wiki.update_attributes!(body: 'vey', user: @user)
-          end
-
-          should_change("versions count", from: 0, to: 2) { @wiki.versions.size }
-
-          should "have only 'oi' and 'vey' versions" do
-            assert_equal ['oi', 'vey'], @wiki.versions.collect(&:body)
-          end
-
-          should "have the right user for its versions" do
-            assert_equal [@user, @user], @wiki.versions.collect(&:user)
-          end
-        end
-
-
-
-        context "with four versions" do
-          setup do
-            @wiki = Wiki.create! body: '1111', user: @user
-            @wiki.update_document!(@different_user, 1, '2222')
-            @wiki.update_document!(@user, 2, '3333')
-            @wiki.update_document!(@different_user, 3, '4444')
-          end
-          should_change("versions count", from: 0, to: 4) { @wiki.versions.size }
-
-          should "find version 1 body" do
-            assert_equal '1111', @wiki.versions.find_by_version(1).body
-          end
-
-          should "find version 4 body" do
-            assert_equal '4444', @wiki.versions.find_by_version(4).body
-          end
-
-          context "after a soft revert to an older version" do
-            setup {@wiki.revert_to_version(3, users(:purple)) }
-
-            should "create a new version equal to the older version" do
-              assert_equal '3333', @wiki.versions.find_by_version(5).body
-            end
-
-            should "revert wiki body" do
-              assert_equal '3333', @wiki.body
-            end
-          end
-
-          context "after a hard revert to an older version" do
-            setup {@wiki.revert_to_version!(2, users(:purple))}
-
-            should "revert wiki body" do
-              assert_equal '2222', @wiki.body
-            end
-
-            should "delete all newer versions" do
-              assert_equal 2, @wiki.versions(true).size
-            end
-
-            should "keep the version it was reverted to" do
-              assert_equal '2222', @wiki.versions.find_by_version(2).body
-            end
-          end
-        end
-      end
-    end
+  def setup
+    @wiki = Wiki.new
+    @user = users(:blue)
+    @different_user = users(:red)
   end
+
+  def test_new_wikis_have_no_versions
+    assert @wiki.versions.empty?
+  end
+
+  def test_save_versions
+    #
+    # save first version
+    #
+    assert_difference '@wiki.versions.size' do
+      @wiki.update_attributes!(body: 'hi', user: @user)
+    end
+    assert_latest_body @wiki, 'hi'
+    assert_latest_body_html @wiki, '<p>hi</p>'
+    assert_latest_raw_structure @wiki, wiki_raw_structure_for_n_byte_body(2)
+    assert_no_difference '@wiki.versions.size', 'saving the same body should never produce a new version' do
+      @wiki.update_attributes!(body: 'hi', user: @different_user)
+    end
+
+    #
+    # save second version
+    #
+    assert_difference '@wiki.versions.size' do
+      @wiki.update_attributes!(body: 'hi there', user: @user)
+    end
+
+    #
+    # save third version
+    #
+    @wiki.body = 'hey you'
+    @wiki.user = @user
+    assert_no_difference '@wiki.versions.size' do
+      assert_nothing_raised { @wiki.save! }
+    end
+    assert_latest_body @wiki, 'hey you'
+    assert_latest_body_html @wiki, '<p>hey you</p>'
+    assert_latest_raw_structure @wiki, wiki_raw_structure_for_n_byte_body(7)
+  end
+
+  def test_initial_empty_body
+    assert_no_new_versions_on_empty_body('')
+  end
+
+  def test_initial_nil_body
+    assert_no_new_versions_on_empty_body(nil)
+  end
+
+  #
+  # save with 'oi', '' (blank body) and 'vey' bodies by alternating users
+  #
+  def test_save_empty_body
+    assert_difference '@wiki.versions.size', 2 do
+      @wiki.update_attributes!(body: 'oi', user: @user)
+      @wiki.update_attributes!(body: '', user: @different_user)
+      @wiki.update_attributes!(body: 'vey', user: @user)
+    end
+
+    assert_equal ['oi', 'vey'], @wiki.versions.collect(&:body),
+      "should have only 'oi' and 'vey' versions"
+
+    assert_equal [@user, @user], @wiki.versions.collect(&:user),
+       "should have the right user for its versions"
+  end
+
+  def test_soft_revert
+    @wiki = Wiki.create! body: '1111', user: @user
+    @wiki.update_attributes!(body: '2222', user: @different_user)
+    @wiki.update_attributes!(body: '3333', user: @user)
+    @wiki.update_attributes!(body: '4444', user: @different_user)
+
+    @wiki.revert_to_version(@wiki.find_version(3), users(:purple))
+    assert_equal '3333', @wiki.versions.find_by_version(5).body,
+      "should create a new version equal to the older version"
+    assert_equal '3333', @wiki.body,
+      "should revert wiki body"
+  end
+
+  def test_hard_revert
+    @wiki = Wiki.create! body: '1111', user: @user
+    @wiki.update_attributes!(body: '2222', user: @different_user)
+    @wiki.update_attributes!(body: '3333', user: @user)
+    @wiki.update_attributes!(body: '4444', user: @different_user)
+
+    @wiki.revert_to_version!(2, users(:purple))
+    assert_equal '2222', @wiki.body,  "should revert wiki body"
+    assert_equal 2, @wiki.versions(true).size, "should delete all newer versions"
+    assert_equal '2222', @wiki.versions.find_by_version(2).body, "should keep version 2"
+  end
+
+  private
+
+  def assert_no_new_versions_on_empty_body(initial_body)
+    @wiki = Wiki.new
+    @wiki.update_attributes!(body: initial_body, user: @user)
+
+    assert_latest_body @wiki, initial_body
+    assert_latest_body_html @wiki, ''
+    assert_latest_raw_structure @wiki, wiki_raw_structure_for_n_byte_body(0)
+
+    #
+    # save from different user
+    #
+    assert_no_difference '@wiki.versions.size' do
+      @wiki.update_attributes!(body: 'oi', user: @different_user)
+    end
+    assert_latest_body @wiki, 'oi'
+
+    #
+    # save from same user
+    #
+    @wiki = Wiki.new
+    @wiki.update_attributes!(body: initial_body, user: @user)
+    assert_no_difference '@wiki.versions.size' do
+      @wiki.update_attributes!(body: 'oi', user: @user)
+    end
+    assert_latest_body @wiki, 'oi'
+  end
+
 end
