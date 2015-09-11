73f1a2c8698010e79ca3316c3c754215d5fc5451
Azul <azul@riseup.net>
2015-09-11 13:05:11 +0200

    group my tasks by pages

--- app/controllers/me/tasks_controller.rb
+++ app/controllers/me/tasks_controller.rb
@@ -1,10 +1,14 @@
 class Me::TasksController < Me::BaseController
 
   def index
-    @tasks = tasks_for_view.order('updated_at DESC').limit(200)
+    @pages = pages_with_tasks.order('pages.updated_at DESC').limit(20)
   end
 
   protected
+
+  def pages_with_tasks
+    TaskListPage.with_tasks tasks_for_view
+  end
 
   def tasks_for_view
     current_user.tasks.send(view)
--- extensions/pages/task_list_page/app/models/task_list_page.rb
+++ extensions/pages/task_list_page/app/models/task_list_page.rb
@@ -13,5 +13,11 @@ class TaskListPage < Page
     # no need to instantiate all the tasks. Using sql to build the string.
     tasks.pluck('CONCAT(name,"\t",description)').join "\n"
   end
+
+  # Fetch the pages that for the given task_ids and include the tasks
+  # Now page.tasks will return only the tasks that were in task_ids \o/.
+  def self.with_tasks(task_ids)
+    includes(:tasks).where(tasks: {id: task_ids})
+  end
 end
 
