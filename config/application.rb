51acf593a6ec0729e64231db13aaf65cf475b439
Azul <azul@riseup.net>
2016-01-27 10:18:34 +0100

    upgrade: application.rb Gemfile without :assets now

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,9 @@ require_relative 'boot'
 
 require 'rails/all'
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
 
 require_relative "../lib/crabgrass/boot.rb"
 require_relative "../lib/crabgrass/public_exceptions.rb"
