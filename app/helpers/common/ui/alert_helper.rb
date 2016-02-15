a77e4e29dfcb14932da01745ca523cb5f86dc202
Azul <azul@riseup.net>
2016-02-15 17:05:03 +0100

    upgrade: use partial to render alert messages
    The main issue this fixes is applying with_indifferent_access to the
    alert hash. Sessions store simple objects only now with the hybrid
    store. So we can't rely on the keys being symbols anymore.

--- app/helpers/common/ui/alert_helper.rb
+++ app/helpers/common/ui/alert_helper.rb
@@ -17,11 +17,7 @@ module Common::Ui::AlertHelper
     # even if ther are not currently any messages to display.
     content_tag(:div, class: 'alert_message_container') do
       content_tag(:div, id: 'alert_messages') do
-        if alert_messages?
-          safe_join(alert_message_strings)
-        else
-          ""
-        end
+        render_alert_messages
       end
     end
   end
@@ -30,17 +26,9 @@ module Common::Ui::AlertHelper
   # generates html for the inline alert messages
   #
   def inline_alert_messages
-    if alert_messages?
-      html = safe_join(alert_message_strings(false))
+    render_alert_messages(false).tap do
       clear_alert_messages # if display inline, we want ensure they are not also floating.
-      html
-    else
-      ""
     end
-  end
-
-  def alert_messages?
-    flash[:messages].present?
   end
 
   def alert_messages_have_errors?
@@ -68,78 +56,17 @@ module Common::Ui::AlertHelper
   # because it also will stop all spinners.
   #
   def update_alert_messages(page)
-    if alert_messages?
-      page.call 'showAlertMessage', safe_join(alert_message_strings)
-    else
-      page.call 'showAlertMessage', ''
-    end
+    page.call 'showAlertMessage', render_alert_messages
   end
 
   ##
   ## DISPLAY
   ##
 
-  def alert_message_strings(allow_fade = true)
-    flash[:messages].collect do |message|
-      message_html(message, allow_fade)
-    end
-  end
-
-  #
-  # generate html for a single message line.
-  #
-  # message is a hash with these keys:
-  #
-  #  :type -- one of :error, :warning, :notice, :success
-  #  :text -- a string or array of strings to display. (optional)
-  #  :list -- an array of strings to be used in a bulleted list
-  #  :fade -- if true, force fading of this message
-  #  :quick -- faster fading
-  #  :nofade -- if true, force no fade
-  #
-  # if allow_fade is false, then we ignore :fade and :nofade options
-  #
-  def message_html(message, allow_fade = true)
-    #icon_class = case message[:type]
-    #  when :error   then 'caution_16'
-    #  when :warning then 'exclamation_16'
-    #  when :notice  then 'lightbulb_16'
-    #  when :success then 'ok_16'
-    #end
-    alert_class = case message[:type]
-      when :error   then 'alert alert-danger'
-      when :warning then 'alert alert-warning'
-      when :notice  then 'alert alert-info'
-      when :success then 'alert alert-success'
-    end
-    message_id = "alert_message_#{rand(100_000_000)}"
-    text = if message[:text].is_a?(Array)
-      if message[:text].size > 1
-        content_tag(:p, message[:text][0], class: 'first') +
-        content_tag(:p, message[:text][1..-1].join)
-      else
-        message[:text].first
-      end
-    else
-      message[:text]
-    end
-    html = []
-    html << link_to_function('×', "hideAlertMessage('#{message_id}')", class: 'close')
-    html << content_tag(:div, text, class: "text")
-    if message[:list]
-      html << content_tag(:ul,
-        safe_join(message[:list].collect{|item|
-          content_tag(:li, item)
-        })
-      )
-    end
-    if allow_fade
-      if message[:fade] || message[:quick] || ((message[:type] == :success || message[:type] == :notice) && !message[:nofade])
-        timeout = message[:quick] ? 0.5 : FADE_TIMEOUT
-        html << content_tag(:script, "hideAlertMessage('#{message_id}', #{timeout});".html_safe)
-      end
-    end
-    content_tag(:div, html.join.html_safe, class: alert_class, id: message_id)
+  def render_alert_messages(allow_fade = true)
+    render partial: 'ui/alert',
+      collection: flash[:messages],
+      locals: {allow_fade: allow_fade, timeout: FADE_TIMEOUT}
   end
 
 end
