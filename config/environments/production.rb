79c26d07c7f9f9c897ef888c4740e250e5f1485e
Azul <azul@riseup.net>
2017-02-04 07:57:00 +0100

    Revert "bugfix: set correct path for assets manifest"
    This reverts commit 95f4d408426a2c3c3e12451d686ddc7c0bb66dcc.
    
    The manifest nowerdays is specified as a filename - not as a path.
    So this caused problems in production.
    
    Somehow the original problem in production is gone never the less:
    The icon_png stylesheet is not referrenced with an integrity hash
    in the html head. No idea what caused sprockets to kick in in the end.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Crabgrass::Application.configure do
   config.assets.digest = true
 
   # Defaults to Rails.root.join("public/assets")
-  config.assets.manifest = Rails.root.join "public/static"
+  # config.assets.manifest = YOUR_PATH
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
