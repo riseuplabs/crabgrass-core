10783e0675f0671297474c75d98fb07896e9b375
Azul <azul@riseup.net>
2017-09-19 09:43:33 +0200

    minor: separate spinner helper, use IMAGE_SIZES

--- app/helpers/common/ui/image_helper.rb
+++ app/helpers/common/ui/image_helper.rb
@@ -1,5 +1,5 @@
 ##
-## Here in lies all the helpers for displaying icons, avatars, spinners,
+## Here in lies all the helpers for displaying icons, avatars
 ## and various images.
 ##
 
@@ -32,88 +32,6 @@ module Common::Ui::ImageHelper
     content_tag :i, ' ', class: "page_icon #{page.icon}_16"
   end
 
-  ##
-  ## SPINNER
-  ##
-  ## spinners are animated gifs that are used to show progress.
-  ## see JavascriptHelper for showing and hiding spinners.
-  ##
-
-  #
-  # returns a spinner tag.
-  # If this is in a ujs remote form it will automagically start and stop
-  # spinning as the form is submitted.
-  #
-  # arguments:
-  #
-  #  id -- unique name of the spinner
-  #  options -- hash of optional options
-  #
-  # options:
-  #
-  #  :show -- if true, default the spinner to be visible
-  #  :align -- override the default vertical alignment. generally, should use the default except in <TD> elements with middle vertical alignment.
-  #  :class -- add css classes to the spinner
-  #  :spinner -- used a different image for the spinner
-  #
-  def spinner(id = nil, options = {})
-    display = ('display:none;' unless options.delete(:show))
-    align = "vertical-align:#{options[:align] || 'middle'}"
-    options.reverse_merge! spinner: 'spinner.gif',
-                           style: "#{display} #{align};",
-                           class: 'spin',
-                           id: id && spinner_id(id),
-                           alt: ''
-    options[:src] = "/images/#{options.delete(:spinner)}"
-    tag :img, options
-  end
-
-  def text_spinner(text, id, options = {})
-    span_options = {
-      id: spinner_id(id),
-      style: ('display:none;' unless options.delete(:show)),
-      class: 'spin'
-    }
-    content_tag :span, span_options do
-      options[:style] = 'vertical-align:baseline'
-      spinner(nil, options) + text
-    end
-  end
-
-  def spinner_id(id)
-    if id.is_a? ActiveRecord::Base
-      id = dom_id(id, 'spinner')
-    else
-      "#{id}_spinner"
-    end
-  end
-
-  def spinner_icon_on(icon, id)
-    target = id ? "$('#{id}')" : 'eventTarget(event)'
-    "replaceClassName(#{target}, '#{icon}_16', 'spinner_icon')"
-  end
-
-  def spinner_icon_off(icon, id)
-    target = id ? "$('#{id}')" : 'eventTarget(event)'
-    "replaceClassName(#{target}, 'spinner_icon', '#{icon}_16')"
-  end
-
-  def big_spinner
-    content_tag :div, '', style: 'background: white url(/images/spinner-big.gif) no-repeat 50% 50%; height: 5em;', class: 'spin'
-  end
-
-  # we can almost do this to trick ie into working with event.target,
-  # which would eliminate the need for random ids.
-  #
-  # but it doesn't quite work, because for :complete of ajax, window.event
-  # is not right
-  #
-  #  function eventTarget(event) {
-  #    event = event || window.event; // IE doesn't pass event as argument.
-  #    return(event.target || event.srcElement); // IE doesn't use .target
-  #  }
-  #
-  # however, this can be used for non-ajax js.
 
   ##
   ## ASSET THUMBNAILS
@@ -270,11 +188,8 @@ module Common::Ui::ImageHelper
       if media.respond_to?(:thumbnail)
         thumbnail = media.thumbnail(size)
         if thumbnail.nil? or thumbnail.failure?
-          dims = case size
-                 when :small  then '64x64'
-                 when :medium then '200x200'
-                 when :large  then '500x500'
-                 end
+          pixels = IMAGE_SIZES[size]
+          dims = pixels.to_s + 'x' + pixels.to_s
           image_tag('/images/ui/corrupted/corrupted.png', size: dims)
         else
           image_tag(thumbnail.url, height: thumbnail.height, width: thumbnail.width)
