e5d66a95f9066b2c27e7defa53a7785fb25b4561
Cognata <cognata@riseup.net>
2017-10-01 23:25:12 +0200

    Add test

--- test/integration/group_creation_test.rb
+++ test/integration/group_creation_test.rb
@@ -10,7 +10,7 @@ class GroupCreationTest < IntegrationTest
 
   def test_create_group
     visit_new_group_form
-    create_group
+    create_group :group, name: 'new_organization'
     assert_group_created
   end
 
@@ -21,16 +21,18 @@ class GroupCreationTest < IntegrationTest
   end
 
   def test_create_committee
-    visit_new_committee_form :animals
-    create_group :committee, parent: groups(:animals), name: 'cold'
+    visit_new_group_form :committee
+    click_link groups(:cnt).name
+    create_group :committee, name: 'new_committee', parent: groups(:cnt)
     assert_group_created
   end
 
   def test_create_council
     visit_new_group_form
-    create_group
-    visit_new_committee_form @group.name, council: true
-    create_group :council, parent: @group
+    create_parent_group :group, name: 'parent_group'
+    visit_new_group_form :council
+    click_link @parent_group.name
+    create_group :council, name: 'new_council', parent: @parent_group
     assert_group_created
   end
 
@@ -43,12 +45,6 @@ class GroupCreationTest < IntegrationTest
     click_on type.to_s.pluralize.capitalize
   end
 
-  def visit_new_committee_form(group, options = {})
-    visit "/groups/#{group}/structure"
-    type = options[:council] ? 'Council' : 'Committee'
-    click_link "Create a new #{type}"
-  end
-
   def create_group(type = :group, attrs = {})
     member = attrs.delete :member
     @group = FactoryGirl.build type, attrs
@@ -57,6 +53,14 @@ class GroupCreationTest < IntegrationTest
     # include parent in name
     @group.clean_names
     select member, from: 'Group' if member
+    select 'English', from: 'Language'
+    click_on 'Create'
+  end
+
+  def create_parent_group(type = :group, attrs = {})
+    @parent_group = FactoryGirl.build type, attrs
+    fill_in 'Name', with: @parent_group.name
+    fill_in 'Display Name', with: @parent_group.display_name
     click_on 'Create'
   end
 
