8a467cbd3525b3d097798d832c8d8e9a6766fec4
dgt <digitaria@riseup.net>
2018-01-04 14:00:58 +0100

    fix: do not call obsolete method link_to_with_confirm

--- app/helpers/group/links_helper.rb
+++ app/helpers/group/links_helper.rb
@@ -87,9 +87,9 @@ module Group::LinksHelper
       if RequestToDestroyOurGroup.already_exists?(group: @group)
         '' # i guess do nothing?
       elsif may_destroy_group?
-        link_to_with_confirm(:destroy_thing.t(thing: @group.display_name),
-                             { confirm: :destroy_confirmation.t(thing: @group.name),
-                               url: direct_group_path(@group), method: :delete }, class: 'btn btn-danger')
+        link_to(:destroy_thing.t(thing: @group.display_name),
+                { confirm: :destroy_confirmation.t(thing: @group.name),
+                  url: direct_group_path(@group), method: :delete }, class: 'btn btn-danger')
       elsif may_create_destroy_request?
         link_to(:destroy_thing.t(thing: @group.display_name),
                 group_requests_path(@group, type: 'destroy_group'),
