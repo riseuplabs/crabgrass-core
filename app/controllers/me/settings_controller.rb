1ab53350c804ff5fce9995956b4a62e42cdfaf40
dgt <digitaria@riseup.net>
2018-01-08 13:48:00 +0100

    fix: send notification emails only if a key was changed
    now a key can be deleted
    by entering an empty string or any string which
    is not a valid pgp public key into the form

--- app/controllers/me/settings_controller.rb
+++ app/controllers/me/settings_controller.rb
@@ -2,11 +2,20 @@ class Me::SettingsController < Me::BaseController
   rescue_render update: :show
 
   def show
-    @user.pgp_key ||= PgpKey.new
+    @user.pgp_key || @user.build_pgp_key
   end
 
   def update
-    current_user.update_attributes!(user_params)
+    params = user_params.to_h
+    if @user.pgp_key and params["pgp_key_attributes"]
+      params["pgp_key_attributes"]["id"] = @user.pgp_key.id
+      params_with_id = ActionController::Parameters.new(params)
+      params_with_id.permit!
+      current_user.update_attributes!(params_with_id)
+    else
+      current_user.update_attributes!(user_params)
+    end
+    PgpKey.delete(@user.pgp_key.id) if @user.pgp_key and @user.pgp_key.fingerprint.blank?
     session[:language_code] = current_user.language if current_user.language
     success
     redirect_to me_settings_url
@@ -18,4 +27,5 @@ class Me::SettingsController < Me::BaseController
     params.require(:user).permit :login, :display_name,
                                  :email, :receive_notifications, :language, :time_zone, pgp_key_attributes: [:key]
   end
+
 end
