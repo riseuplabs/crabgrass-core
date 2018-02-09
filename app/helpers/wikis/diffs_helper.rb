493aebe6b3fb9e454d4a8a3e1e5037d916e84b1a
dgt <digitaria@riseup.net>
2018-02-09 16:13:16 +0100

    Fix: bring back previous and next button for wiki diff

--- app/helpers/wikis/diffs_helper.rb
+++ app/helpers/wikis/diffs_helper.rb
@@ -25,4 +25,24 @@ module Wikis::DiffsHelper
                 class: 'diffins', style: 'padding: 1px 4px;'
   end
 
+  def diff_previous_link
+    if previous = @old.previous
+      link_to_remote (LARROW + :prev_change.t).html_safe,
+                     url: wiki_diff_path(@wiki, @old.diff_id),
+                     method: :get
+    else
+      content_tag :span, (LARROW + :prev_change.t).html_safe, class: 'disabled'
+    end
+  end
+
+  def diff_next_link
+    if next_version = @new.next
+      link_to_remote (:next_change.t + RARROW).html_safe,
+                     url: wiki_diff_path(@wiki, next_version.diff_id),
+                     method: :get
+    else
+      content_tag :span, (:next_change.t + RARROW).html_safe, class: 'disabled'
+    end
+  end
+
 end
