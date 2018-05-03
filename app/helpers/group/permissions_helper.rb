2dd32f6215ea4e220fcf4318abdd03d6aa1eb841
dgt <digitaria@riseup.net>
2018-05-03 19:18:15 +0200

    Fix: correct translation key lookup for things (mainly group types)
    key lookup failed because group_type had to be downcased
    the model key group: organization had to be removed to make english translation of group_type work - groups
    are now groups and not organzations - no sideffects detected yet.
    small additions and changes to english translations

--- app/helpers/group/permissions_helper.rb
+++ app/helpers/group/permissions_helper.rb
@@ -1,8 +1,8 @@
 module Group::PermissionsHelper
   def publicly_visible_checkbox(form)
     form.row do |r|
-      r.input castle_gate_tag(@group, :view, @holders, label: :group_publicly_visible.t(group: @group.group_type))
-      r.info :group_publicly_visible_description.t(domain: current_site.domain, group: @group.group_type.capitalize)
+      r.input castle_gate_tag(@group, :view, @holders, label: :group_publicly_visible.t(group: t(@group.group_type.downcase)))
+      r.info :group_publicly_visible_description.t(domain: current_site.domain, group: t(@group.group_type.downcase))
     end
   end
 
@@ -25,44 +25,31 @@ module Group::PermissionsHelper
   def group_members_publicly_visible_checkbox(form)
     form.row(disabled: group_hidden?, class: 'depends_on_view') do |r|
       r.input castle_gate_tag(@group, :see_members, @holders, label: :group_members_publicly_visible.t)
-      r.info :group_members_publicly_visible_description.t(domain: current_site.domain, group: @group.group_type.capitalize)
+      r.info :group_members_publicly_visible_description.t(domain: current_site.domain, group: t(@group.group_type.downcase))
     end
   end
 
   def allow_membership_requests_checkbox(form)
     form.row(class: 'depends_on_view') do |r|
       r.input castle_gate_tag(@group, :request_membership, @holders, label: :allow_membership_requests.t)
-      r.info :may_request_membership_description.t(group: @group.group_type)
+      r.info :may_request_membership_description.t(group: t(@group.group_type.downcase))
     end
   end
 
   def open_membership_policy_checkbox(form)
     return if @group.council?
     form.row(class: 'depends_on_request_membership depends_on_view', disabled: group_closed?) do |r|
-      r.input castle_gate_tag(@group, :join, @holders, label: :open_group.t(group: @group.group_type))
-      r.info :open_group_description.t(group: @group.group_type)
+      r.input castle_gate_tag(@group, :join, @holders, label: :open_group.t(group: t(@group.group_type.downcase)))
+      r.info :open_group_description.t(group: t(@group.group_type.downcase))
     end
   end
 
   def members_may_edit_wiki_checkbox(form)
     form.row do |r|
       r.input castle_gate_tag(@group, :edit, [CastleGates::Holder[@group]], label: :members_may_edit_wiki.t)
-      r.info :members_may_edit_wiki_description.t(group: @group.group_type)
+      r.info :members_may_edit_wiki_description.t(group: t(@group.group_type.downcase))
     end
   end
-
-  # def council_field(f)
-  #   f.row do |row|
-  #     if @group.council_id
-  #       row.input link_to_group(@group.council, :avatar => :small)
-  #     else
-  #       row.input link_to(
-  #         :create_a_new_thing.t(:thing => I18n.t(:council).downcase),
-  #         new_group_council_path(@group))
-  #       row.info :council_description_details.t(:group => @group.group_type)
-  #     end
-  #   end
-  # end
 
   def group_hidden?
     !@group.access?(public: :view)
