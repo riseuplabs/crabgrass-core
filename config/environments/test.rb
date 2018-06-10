92d0559321da4989155971a7b5d6275412c62571
Azul <azul@riseup.net>
2018-06-11 01:10:50 +0200

    random test order

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,6 +26,9 @@ Crabgrass::Application.configure do
   # database-specific column types
   # config.active_record.schema_format = :sql
 
+  # rails 5 like behaviour
+  config.active_support.test_order = :random
+
   ##
   ## CRABGRASS OPTIONS
   ##
