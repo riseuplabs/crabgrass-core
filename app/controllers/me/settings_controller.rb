95bfc0e953eb404602885a320b5515535db0d975
Azul <azul@riseup.net>
2019-05-04 15:32:42 +0200

    update: user permit instead of permit! for user settings

--- app/controllers/me/settings_controller.rb
+++ app/controllers/me/settings_controller.rb
@@ -6,15 +6,7 @@ class Me::SettingsController < Me::BaseController
   end
 
   def update
-    params = user_params.to_h
-    if @user.pgp_key and params["pgp_key_attributes"]
-      params["pgp_key_attributes"]["id"] = @user.pgp_key.id
-      params_with_id = ActionController::Parameters.new(params)
-      params_with_id.permit!
-      current_user.update_attributes!(params_with_id)
-    else
-      current_user.update_attributes!(user_params)
-    end
+    current_user.update_attributes!(user_params)
     PgpKey.delete(@user.pgp_key.id) if @user.pgp_key and @user.pgp_key.fingerprint.blank?
     session[:language_code] = current_user.language if current_user.language
     success
@@ -24,8 +16,22 @@ class Me::SettingsController < Me::BaseController
   protected
 
   def user_params
-    params.require(:user).permit :login, :display_name,
-                                 :email, :receive_notifications, :language, :time_zone, pgp_key_attributes: [:key]
+    params.require(:user).
+      merge(pgp_key_attributes: pgp_key_attributes_with_id).
+      permit( :login,
+              :display_name,
+              :email,
+              :receive_notifications,
+              :language,
+              :time_zone,
+              pgp_key_attributes: [:key, :id] )
   end
 
+  # In order to update existing keys
+  # or to remove them by submitting an empty form
+  # we need to add the id of the previous key.
+  def pgp_key_attributes_with_id
+    from_params = params.require(:user).fetch("pgp_key_attributes", nil)
+    return from_params.merge(id: @user.pgp_key.try.id) if from_params
+  end
 end
