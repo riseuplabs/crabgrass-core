c29a0c1fefa68428f5b0d6c7f873afce34dc2071
Azul <azul@riseup.net>
2019-05-10 23:02:38 +0200

    test: actually trigger requests
    also turned instance vars into local vars
    as they were not reused anywhere.

--- test/functional/me/requests_controller_test.rb
+++ test/functional/me/requests_controller_test.rb
@@ -15,33 +15,35 @@ class Me::RequestsControllerTest < ActionController::TestCase
   end
 
   def test_approve_friend_request
-    @user = FactoryBot.create(:user)
+    user = FactoryBot.create(:user)
     requesting = FactoryBot.create(:user)
     request = RequestToFriend.create created_by: requesting,
-                                     recipient: @user
-    login_as @user
+                                     recipient: user
+    login_as user
+    xhr :post, :update, id: request.id, mark: 'approve'
     assert_response :success
   end
 
   def test_approve_group_request
-    @user = FactoryBot.create(:user)
-    @group = FactoryBot.create(:group)
-    @group.add_user! @user
-    login_as @user
+    user = FactoryBot.create(:user)
+    group = FactoryBot.create(:group)
+    group.add_user! user
+    login_as user
     requesting = FactoryBot.create(:user)
     request = RequestToJoinYou.create created_by: requesting,
-                                      recipient: @group
+                                      recipient: group
+    xhr :post, :update, id: request.id, mark: 'approve'
     assert_response :success
   end
 
   def test_destroy_group_request
-    @user = FactoryBot.create(:user)
-    @group = FactoryBot.create(:group)
-    @group.add_user! @user
-    login_as @user
+    user = FactoryBot.create(:user)
+    group = FactoryBot.create(:group)
+    group.add_user! user
+    login_as user
     requesting = FactoryBot.create(:user)
     request = RequestToJoinYou.create created_by: requesting,
-                                      recipient: @group
+                                      recipient: group
     assert_difference 'RequestToJoinYou.count', -1 do
       xhr :delete, :destroy, id: request.id
     end
@@ -49,8 +51,8 @@ class Me::RequestsControllerTest < ActionController::TestCase
   end
 
   def test_other_requests_hidden
-    @user = FactoryBot.create(:user)
-    login_as @user
+    user = FactoryBot.create(:user)
+    login_as user
     assert_not_found do
       get :show, id: Request.last
     end
