8c31544ad011cc24797ae842e115542c56b376c7
Azul <azul@riseup.net>
2019-05-22 23:10:07 +0200

    fix: printer friendly view for wiki and discussion pages
    including tests

--- extensions/pages/discussion_page/test/functional/discussion_page_controller_test.rb
+++ extensions/pages/discussion_page/test/functional/discussion_page_controller_test.rb
@@ -1,13 +1,17 @@
 require 'test_helper'
 
 class DiscussionPageControllerTest < ActionController::TestCase
-  def setup
-    @request.host = 'localhost'
-  end
 
   def test_show
     page = DiscussionPage.where(public: true).first
     get :show, params: { id: page.id }
+    assert_response :success
+  end
+
+  def test_print
+    login_as users(:blue)
+    page = pages(:committee_page)
+    get :print, params: { id: page.id }
     assert_response :success
   end
 end
