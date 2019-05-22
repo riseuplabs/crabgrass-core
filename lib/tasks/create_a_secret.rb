56b5c9ab605874401420d27803ed1368ee137e72
Azul <azul@riseup.net>
2019-05-22 06:43:35 +0200

    cleanup: create_a_secret task is not used anymore

--- lib/tasks/create_a_secret.rb
+++
@@ -1,6 +0,0 @@
-task :create_a_secret do
-  puts 'Crabgrass now uses the default rails4 mechanism for storing secrets.'
-  puts "Please run 'rake secret' and copy the key to the line for your"
-  puts 'environment in config/secrets.yml.'
-  puts 'For production you can also set the environments SECRET_KEY_BASE.'
-end
