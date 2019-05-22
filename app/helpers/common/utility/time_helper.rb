09d43878406301233e7209b622f525180e867f9c
Azul <azul@riseup.net>
2019-05-22 10:35:07 +0200

    cleanup: remove unused utility helper functions

--- app/helpers/common/utility/time_helper.rb
+++ app/helpers/common/utility/time_helper.rb
@@ -59,77 +59,11 @@ module Common::Utility::TimeHelper
     end
   end
 
-  def localize_month(month)
-    # for example => :month_short_january
-    month_sym = ('month_short_' + month.downcase).to_sym
-    I18n.t(month_sym)
-  end
-
   # formats a time, in full detail
   # for example: Sunday 2007/July/3 2:13PM PST
   def full_time(time)
     I18n.locale == :en ?
       time.strftime('%A %Y/%b/%d %I:%M%p') :
       I18n.l(time)
-  end
-
-  def full_date(date)
-    date.strftime('%A %d/%b/%Y')
-  end
-
-  def to_utc(time)
-    Time.zone.local_to_utc(time)
-  end
-
-  def local_now
-    Time.zone.now
-  end
-
-  def after_day_start?(time)
-    local_now.at_beginning_of_day < time
-  end
-
-  def after_yesterday_start?(time)
-    local_now.yesterday.at_beginning_of_day < time
-  end
-
-  def after_week_start?(time)
-    (local_now.at_beginning_of_day - 7.days) < time
-  end
-
-  ##
-  ## This are used as a cheap time based expiry of fragment caches.
-  ##
-  ## for example:
-  ##   cache(:expires_in => hours(3))
-  ##
-  ## this does NOT actually expire the fragment cache, but it makes it invalid
-  ## after three hours, which is just as good so long as you have an external
-  ## job that cleans up after old files.
-  ##
-
-  def hours(num)
-    (Time.now.to_i / num.hour).floor
-  end
-
-  def days(num)
-    (Time.now.to_i / num.days).floor
-  end
-
-  ##############################################
-  ## UI helpers
-
-  def calendar_tag(_field_id, date = nil)
-    include_calendar_tags
-    calendar_date_select_tag(date ? date.to_date.to_formatted_s(:long) : nil)
-  end
-
-  def include_calendar_tags
-    unless @calendar_tags_included
-      @calendar_tags_included = true
-      content_for :end_tags do
-        calendar_date_select_includes 'default'
-      end
-    end
   end
 end
