7368cedf091fcb641c73eaa9dff39089800ff91f
Azul <azul@riseup.net>
2019-05-23 06:32:06 +0200

    cleanup: unused survey page views, helpers and controller code

--- extensions/pages/survey_page/app/controllers/survey_page_controller.rb
+++ extensions/pages/survey_page/app/controllers/survey_page_controller.rb
@@ -1,15 +1,10 @@
-
 class SurveyPageController < Page::BaseController
   helper 'survey_page'
 
   def show
     authorize @page
-    if @page.data.nil?
-      redirect_to page_url(@page, action: 'edit')
-    else
-      @survey.responses(true)
-      # ^^ there is no good reason why this is necessary, but it seems to be the case.
-    end
+    @survey.responses(true)
+    # ^^ there is no good reason why this is necessary, but it seems to be the case.
   end
 
   protected
