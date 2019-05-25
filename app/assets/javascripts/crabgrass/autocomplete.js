0fbc7c25365c3de070c70dfa6ac1ba1feea68261
Azul <azul@riseup.net>
2019-05-25 22:00:29 +0200

    cleanup: remove unused javascript

--- app/assets/javascripts/crabgrass/autocomplete.js
+++ app/assets/javascripts/crabgrass/autocomplete.js
@@ -16,18 +16,18 @@ function cgAutocompleteEntities(id, url, opts) {
      rowRenderer: autoCompleteRowRenderer,
      selectValue: autoCompleteSelectValue
    };
-   if (opts) { Object.extend(options, opts); }
-   new Autocomplete(id, options, random_id);
-}
+  if (opts) { Object.extend(options, opts); }
+  new Autocomplete(id, options, random_id);
 
-function autoCompleteRowRenderer(value, re, data) {
-  return "<p class='icon xsmall' style='background-image: url(/avatars/" + data + "/xsmall.jpg)'>" + value.replace(/^<em>(.*)<\/em>(<br\/>(.*))?$/gi, function(m, m1, m2, m3){return "<em>" + Autocomplete.highlight(m1,re) + "</em>" + (m3 ? "<br/>" + Autocomplete.highlight(m3, re) : "")}) + "</p>";
-}
+  function autoCompleteRowRenderer(value, re, data) {
+    return "<p class='icon xsmall' style='background-image: url(/avatars/" + data + "/xsmall.jpg)'>" + value.replace(/^<em>(.*)<\/em>(<br\/>(.*))?$/gi, function(m, m1, m2, m3){return "<em>" + Autocomplete.highlight(m1,re) + "</em>" + (m3 ? "<br/>" + Autocomplete.highlight(m3, re) : "")}) + "</p>";
+  }
 
-function autoCompleteSelectValue(value){
-  // if there are two rows pick the second one
-  row = value.replace(/.*<br\/>(.*)/g,'$1');
-  // encode text without the tags
-  return row.replace(/<[^>]*>/g,'');
-//  return encodeURIComponent(row.replace(/<[^>]*>/g,''));
+  function autoCompleteSelectValue(value){
+    // if there are two rows pick the second one
+    row = value.replace(/.*<br\/>(.*)/g,'$1');
+    // encode text without the tags
+    return row.replace(/<[^>]*>/g,'');
+    //  return encodeURIComponent(row.replace(/<[^>]*>/g,''));
+  }
 }
--- app/assets/javascripts/crabgrass/crabgrass.js
+++ app/assets/javascripts/crabgrass/crabgrass.js
@@ -49,16 +49,6 @@ function replaceClassName(element, old_class, new_class) {
 }
 
 //
-// replaces an element with a new one if it is empty.
-//
-function replaceIfEmpty(selector, newElement) {
-  var el = $$(selector).first()
-  if (el && el.empty()) {
-    el.replace(newElement);
-  }
-}
-
-//
 // EVENTS
 //
 
@@ -101,41 +91,4 @@ var LocationHash = {
     if (!this.polling) { setInterval("LocationHash.poll()", 300) }
     this.polling = true;
   }
-}
-
-//
-// split panel
-//
-
-function activatePanelRow(row_id) {
-  // reset styles
-  $$('.panel_right .row').invoke('hide');
-  $$('.panel_arrow').invoke('hide');
-  $$('.panel_left .row').invoke('removeClassName', 'active');
-
-  if (row_id) {
-    // highlight left panel row
-    $('panel_left_'+row_id).addClassName('active');
-    var halfHeight = $('panel_left_'+row_id).getHeight() / 2 + "px";
-    var borderWidthStr = "#{top} #{right} #{bottom} #{left}".interpolate({top: halfHeight, right:"0px", bottom: halfHeight, left:"10px"});
-    $('panel_arrow_'+row_id).setStyle({borderWidth: borderWidthStr, display: 'block'});
-
-    // position and show right panel row
-    var offset = $('panel_left_'+row_id).offsetTop + 'px';
-    $$('.panel_right').first().setStyle({paddingTop:offset})
-    $('panel_right_'+row_id).show();
-  }
-}
-
-//
-// sliding list
-//
-// left and right contain these keys:
-//   path  -- the url path of the panel. this is used both for history
-//           and loading content via ajax.
-//   domid -- dom id of the element to update
-//
-function activateSlidingRow(left, right) {
-  History.replaceState({slide_right:'sliding-list', update:left}, null, left.path);
-  History.pushState({slide_left:'sliding-list', update:right}, null, right.path);
 }
